be9d79840f3546bd8800c8edfdc22b9f849eb0c6
Cayman Unterborn <kmanunterborn@gmail.com>
2012-12-03 00:28:44 +0000

    renamed a file to make more clear what example it actually is
    git-svn-id: https://burnman.googlecode.com/svn/trunk@44 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- example_composition.py
+++ example_composition.py
@@ -13,6 +13,9 @@ from code import main as main
 #INPUT for method
 method = 'slb' # choose 'slb' (finite-strain, stixrude and lithgow-bertelloni, 2005) or 'mgd' (mie-gruneisen-debeye, matas et al. 2007)
 
+#INPUT for geotherm
+geotherm = 'geotherm_brown_shankland'
+
 
 #Example 1: simple fixed minerals
 if False:
@@ -27,6 +30,7 @@ if True:
 	molar_abundances = ( amount_perovskite, 1.0-amount_perovskite)
 
 #Example 3: input weight percentages
+#See comments in code/composition.py for references to partition coefficent calculation
 if False:
 	weight_percents = {'Mg':0.213, 'Fe': 0.0626, 'Si':0.242, 'Ca':0., 'Al':0.}
 	phase_fractions,relative_molar_percent = part.conv_inputs(weight_percents)
@@ -51,7 +55,7 @@ depth_step = 100.0 				# steps at which the seismic velocity and calculated velo
 seis_p, seis_r, seis_vp, seis_vphi, seis_vs, seis_rho = main.seismo_load(name, attenuation_correction, depth_min, depth_max,depth_step)
  
         
-temperature = main.build_geotherm('geotherm_brown_shankland', seis_p)
+temperature = main.build_geotherm(geotherm, seis_p)
 
 for ph in phases:
 	ph.set_method(method)
