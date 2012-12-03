ddbf9630418e4c1f83fc0387de0acafe3f4190d1
Timo Heister <timo.heister@gmail.com>
2012-12-03 00:53:35 +0000

    simplify example
    git-svn-id: https://burnman.googlecode.com/svn/trunk@50 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- example_woutput.py
+++ example_woutput.py
@@ -22,14 +22,11 @@ phases = (minerals.mg_fe_perovskite(0.7), minerals.ferropericlase(0.5))
 amount_perovskite = 0.95
 molar_abundances = ( amount_perovskite, 1.0-amount_perovskite)
 
+#define some pressure range
+pressures = np.arange(25e9,130e9,5e9)
 
-seis_p = np.arange(25e9,130e9,5e9)
- 
-#input the output filename
-output_filename = "example_woutput.txt"
-   
 geotherm = main.get_geotherm("brown_shankland")
-temperature = [geotherm(p) for p in seis_p]
+temperature = [geotherm(p) for p in pressures]
 
 for ph in phases:
 	ph.set_method(method)
@@ -38,15 +35,16 @@ print "Calculations are done for:"
 for i in range(len(phases)):
 	print molar_abundances[i], " of phase", phases[i].to_string()
 
-mat_rho, mat_vs, mat_vp, mat_vphi, mat_K, mat_mu = main.calculate_velocities(seis_p, temperature, phases, molar_abundances)	
+mat_rho, mat_vs, mat_vp, mat_vphi, mat_K, mat_mu = main.calculate_velocities(pressures, temperature, phases, molar_abundances)	
 	
 #write to file:
-
+output_filename = "example_woutput.txt" 
 file = open(output_filename, 'wb')
-file.write("Pressure	Temperature	mat_rho		mat_vs		mat_vp		mat_vphi	mat_K		mat_mu\n")
-writer = csv.writer(file, delimiter="\t")
-data = np.array([seis_p,temperature,mat_rho, mat_vs, mat_vp, mat_vphi, mat_K, mat_mu]).transpose()
-writer.writerows(data)
+file.write("Pressure\tTemperature\tmat_rho\tmat_vs\tmat_vp\tmat_vphi\tmat_K\tmat_mu\n")
+
+data = zip(pressures,temperature,mat_rho, mat_vs, mat_vp, mat_vphi, mat_K, mat_mu)
+np.savetxt(file,data,fmt='%.10e',delimiter='\t')
+
 	
 print "\nYour data has been saved as: ",output_filename
 
