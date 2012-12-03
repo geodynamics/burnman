e6d63f387264783c5d9b79fcbe4ff98562a26d2e
Cayman Unterborn <kmanunterborn@gmail.com>
2012-12-03 01:05:39 +0000

    added comparing two models example
    git-svn-id: https://burnman.googlecode.com/svn/trunk@53 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

---
+++ example_compare_two_models.py
@@ -0,0 +1,103 @@
+import os, sys, numpy as np
+import matplotlib.pyplot as plt
+
+lib_path = os.path.abspath('code/')
+sys.path.append(lib_path)
+
+from code import minerals 
+from code import main as main
+
+###Input Model 1
+
+##input method
+method_1 = 'slb' # choose 'slb' (finite-strain, stixrude and lithgow-bertelloni, 2005) or 'mgd' (mie-gruneisen-debeye, matas et al. 2007)
+
+
+#Input composition of model 1. See example_composition for potential choices. We'll just choose something simple here
+	
+phases_1 = (minerals.Murakami_perovskite(), minerals.Murakami_fp_LS())
+amount_perovskite_1 = 0.95
+molar_abundances_1 = ( amount_perovskite_1, 1.0-amount_perovskite_1)
+
+#input pressure range for first model. This could be from a seismic model or something you create. For this example we will create an array
+
+seis_p_1 = np.arange(25e9, 125e9, 5e9)
+
+#input your geotherm. Either choose one (See example_geotherms.py) or create one.We'll use Brown and Shankland.
+
+geotherm = main.get_geotherm("brown_shankland")
+temperature_1 = [geotherm(p) for p in seis_p_1]
+
+##Now onto the second model parameters
+
+##input second method
+method_2 = 'mgd' # choose 'slb' (finite-strain, stixrude and lithgow-bertelloni, 2005) or 'mgd' (mie-gruneisen-debeye, matas et al. 2007)
+
+
+#Input composition of model 2. See example_composition for potential choices. We'll just choose something simple here
+	
+phases_2 = (minerals.Murakami_perovskite(), minerals.Murakami_fp_LS())
+amount_perovskite_2 = 0.95
+molar_abundances_2 = ( amount_perovskite_1, 1.0-amount_perovskite_1)
+
+#input pressure range for first model. This could be from a seismic model or something you create. For this example we will create an array
+
+seis_p_2 = np.arange(35e9, 130e9, 5e9)
+
+#input your geotherm. Either choose one (See example_geotherms.py) or create one.We'll use Brown and Shankland.
+
+geotherm = main.get_geotherm("brown_shankland")
+temperature_2 = [geotherm(p) for p in seis_p_2]
+
+
+#Now we'll calculate the models. 
+
+for ph in phases_1:
+	ph.set_method(method_1)
+
+print "Calculations are done for:"
+for i in range(len(phases_1)):
+	print molar_abundances_1[i], " of phase", phases_1[i].to_string()
+
+mat_rho_1, mat_vs_1, mat_vp_1, mat_vphi_1, mat_K_1, mat_mu_1 = main.calculate_velocities(seis_p_1, temperature_1, phases_1, molar_abundances_1)	
+
+for ph in phases_2:
+	ph.set_method(method_2)
+
+print "Calculations are done for:"
+for i in range(len(phases_2)):
+	print molar_abundances_2[i], " of phase", phases_2[i].to_string()
+
+mat_rho_2, mat_vs_2, mat_vp_2, mat_vphi_2, mat_K_2, mat_mu_2 = main.calculate_velocities(seis_p_2, temperature_2, phases_2, molar_abundances_2)	
+
+
+##Now let's plot the comparison. You can conversely just output to a data file (see example_woutput.py)
+
+plt.subplot(2,2,1)
+p1,=plt.plot(seis_p_1/1.e9,mat_vs_1,color='b',linestyle='-',marker='o',markerfacecolor='b',markersize=4)
+p2,=plt.plot(seis_p_2/1.e9,mat_vs_2,color='k',linestyle='-',marker='o',markerfacecolor='k',markersize=4)
+plt.title("Vs (km/s)")
+plt.ylim(5.1,7.6)
+
+# plot Vphi
+plt.subplot(2,2,2)
+p1,=plt.plot(seis_p_1/1.e9,mat_vphi_1,color='b',linestyle='-',marker='o',markerfacecolor='b',markersize=4)
+p2,=plt.plot(seis_p_2/1.e9,mat_vphi_2,color='k',linestyle='-',marker='o',markerfacecolor='k',markersize=4)
+plt.title("Vphi (km/s)")
+plt.ylim(7,12)
+#	plt.legend([p1,p2],["Murakami (0.93 Pv, 0.07 fp)", "seismic model (PREM)"], loc=4)
+
+# plot density
+plt.subplot(2,2,3)
+p1,=plt.plot(seis_p_1/1.e9,mat_rho_1,color='b',linestyle='-',marker='o',markerfacecolor='b',markersize=4)
+p2,=plt.plot(seis_p_2/1.e9,mat_rho_2,color='k',linestyle='-',marker='o',markerfacecolor='k',markersize=4)
+plt.title("density (kg/m^3)")
+plt.legend([p1,p2],["model 1", "model 2"], loc=4)
+
+# plot geotherm
+plt.subplot(2,2,4)
+p1,=plt.plot(seis_p_1/1e9,temperature_1,color='r',linestyle='-',marker='o',markerfacecolor='r',markersize=4)
+p2,=plt.plot(seis_p_2/1e9,temperature_2,color='r',linestyle='-',marker='o',markerfacecolor='r',markersize=4)
+plt.title("Geotherm (K)")
+
+plt.show()
