5e499365da59143e5e2c355e179a045a32e03d45
Timo Heister <timo.heister@gmail.com>
2012-12-05 01:40:58 +0000

    switch example_composition to new seismic model
    git-svn-id: https://burnman.googlecode.com/svn/trunk@67 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- example_composition.py
+++ example_composition.py
@@ -6,6 +6,7 @@ sys.path.append(lib_path)
 
 from code import minerals 
 from code import main as main
+from code import seismic
 
 ### input variables ###
 #######################
@@ -14,13 +15,13 @@ from code import main as main
 method = 'slb' # choose 'slb' (finite-strain, stixrude and lithgow-bertelloni, 2005) or 'mgd' (mie-gruneisen-debeye, matas et al. 2007)
 
 #Example 1: simple fixed minerals
-if False:
+if True:
 	phases = (minerals.Murakami_perovskite(), minerals.Murakami_fp_LS())
 	amount_perovskite = 0.95
 	molar_abundances = ( amount_perovskite, 1.0-amount_perovskite)
 
 #Example 2: specify iron content
-if True:
+if False:
 	phases = (minerals.mg_fe_perovskite(0.7), minerals.ferropericlase(0.5))
 	amount_perovskite = 0.95
 	molar_abundances = ( amount_perovskite, 1.0-amount_perovskite)
@@ -41,15 +42,13 @@ if False:
 	molar_abundances = ( 0.7, 0.2, 0.1)
 
 
-#INPUT for seismic models
-name = 'prem'  					# choose from 'prem'(at 1 second, Dziewonski & Anderson, 1981), 'ref_fast' (Lekic et al. 2012), 'ref_slow' (Lekic et al. 2012)
-attenuation_correction= 'off'   		# correction for attuation (not required for PREM at 1s, see Matas et al. 2007, page 4) choose from 'on', 'off'
-depth_min = 0.0   				# minimum depth considered in km, choose 0.0 to use the limits given by the seismic model
-depth_max = 0.0   				# maximum depth considered in km, choose 0.0 to use the limits given by the seismic model
-depth_step = 100.0 				# steps at which the seismic velocity and calculated velocity are compared (in between is interpolated)
+#seismic model for comparison:
+seismic_model = seismic.prem() # pick from .prem() .slow() .fast() (see code/seismic.py)
+number_of_points = 20 #set on how many depth slices the computations should be done
+depths = np.linspace(700,2800, number_of_points)
+#depths = seismic_model.internal_depth_list()
+seis_p, seis_rho, seis_vp, seis_vs, seis_vphi = seismic_model.evaluate_all_at(depths)
 
-seis_p, seis_r, seis_vp, seis_vphi, seis_vs, seis_rho = main.seismo_load(name, attenuation_correction, depth_min, depth_max,depth_step)
- 
         
 geotherm = main.get_geotherm("brown_shankland")
 temperature = [geotherm(p) for p in seis_p]
@@ -63,11 +62,10 @@ for i in range(len(phases)):
 
 mat_rho, mat_vs, mat_vp, mat_vphi, mat_K, mat_mu = main.calculate_velocities(seis_p, temperature, phases, molar_abundances)	
 
-[rho_err,vphi_err,vs_err]=main.compare_with_seismic_model(mat_vs,mat_vphi,mat_rho,seis_vs,seis_vphi,seis_rho/1e3) #check units on rho
+[rho_err,vphi_err,vs_err]=main.compare_with_seismic_model(mat_vs,mat_vphi,mat_rho,seis_vs,seis_vphi,seis_rho)
 	
 
 # PLOTTING
-
 
 plt.subplot(2,2,1)
 p1,=plt.plot(seis_p/1.e9,mat_vs,color='b',linestyle='-',marker='o',markerfacecolor='b',markersize=4,label='computation')
@@ -91,7 +89,7 @@ plt.text(40,11.5,"misfit= %3.3f" % vphi_err)
 # plot density
 plt.subplot(2,2,3)
 p1,=plt.plot(seis_p/1.e9,mat_rho,color='b',linestyle='-',marker='o',markerfacecolor='b',markersize=4)
-p2,=plt.plot(seis_p/1.e9,seis_rho/1.e3,color='k',linestyle='-',marker='o',markerfacecolor='k',markersize=4)
+p2,=plt.plot(seis_p/1.e9,seis_rho,color='k',linestyle='-',marker='o',markerfacecolor='k',markersize=4)
 plt.title("density (kg/m^3)")
 plt.xlim(min(seis_p)/1.e9,max(seis_p)/1.e9)
 plt.text(40,5.3,"misfit= %3.3f" % rho_err)
