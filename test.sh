46dbbdeb3240de5eace544833871875a5f33f3fb
Timo Heister <timo.heister@gmail.com>
2012-12-05 19:12:10 +0000

    script to run all tests
    git-svn-id: https://burnman.googlecode.com/svn/trunk@72 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python example_compare_two_models.py
+python example_geotherms.py
+python example_woutput.py
+python example_optimize_pv.py
+python example_composition.py
+python example_seismic.py
+
+echo tests done
