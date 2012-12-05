fixed bug in v_phi computation
git-svn-id: https://burnman.googlecode.com/svn/trunk@62 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

code/seismo_in.py
@@ -61,7 +61,7 @@ class seismo_in:
     def v_phi(self):
         vs=self.v_s()
         vp=self.v_p()
-        return [np.sqrt(pow(vp[i],2.0)-4/3*pow(vs[i],2.0)) for i in range(len(vp))]
+        return [np.sqrt(pow(vp[i],2.0)-4./3.*pow(vs[i],2.0)) for i in range(len(vp))]
 
 
 