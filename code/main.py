27a8ee5ec09c0211124539392e241643f25b01a9
Timo Heister <timo.heister@gmail.com>
2012-12-06 18:57:35 +0000

    now remove the old seismo_load
    git-svn-id: https://burnman.googlecode.com/svn/trunk@75 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- code/main.py
+++ code/main.py
@@ -11,13 +11,17 @@ from code import geotherm as gt
 from code import material
 from code import mie_grueneisen_debye
 from code import minerals 
-from code import prem
-from code import seismo_in as seis
+from code import seismic
 from code import slb_finitestrain
 from code import tools
 from code import voigt_reuss_hill as vrh
+from code import seismo_in as seis
+
+
 
 def calculate_velocities (pressure, temperature, phases, molar_abundances):
+	attenuation = False # TODO, make parameter?
+
 	mat_vs = np.empty_like(pressure)
 	mat_vp = np.empty_like(pressure)
 	mat_vphi = np.empty_like(pressure)
@@ -28,7 +32,7 @@ def calculate_velocities (pressure, temperature, phases, molar_abundances):
 	#print "seismic p (GPa)	T (K)	density(kg/m^3)	K(Gpa) G(GPa)	Vs (km/s)	Vp(km/s)	Vphi (km/s)"
 	for i in range(len(pressure)):
 		rho,vp,vs,vphi,K,mu = \
-		vrh.voigt_reuss_hill(pressure[i], temperature[i], phases, molar_abundances)
+		vrh.voigt_reuss_hill(pressure[i], temperature[i], phases, molar_abundances, attenuation)
 		#print pressure[i]/1.e9,"	", temperature[i],"	",rho,"	", K,"	", mu,"	", vs/1.e3,"	", vp/1.e3,"	", vphi/1.e3
 		#print pressure[i]/1.e9,"	",rho,"	", mu
 		mat_rho[i] = rho/1.e3
@@ -60,24 +64,6 @@ def chi_factor(calc,obs):
 	err_tot=np.sum(err)/len(err)
 
 	return err_tot
-
-def seismo_load(name,attenuation_correction,depth_min,depth_max,depth_step):
-	s=seis.seismo_in()
-	s.params['name']=name
-	s.params['attenuation_correction']=attenuation_correction
-	s.params['depth_min']=depth_min
-	s.params['depth_max']=depth_max
-	s.params['depth_step']=depth_step
-
-	print "Looking up seismic model:",s.params['name']
-	seis_p = np.array(s.pressure())*1.e9     
-	seis_r = np.array(s.radius())/1.e3
-	seis_vp = np.array(s.v_p())/1.e3#[seis.seis_V(y/1.e9)[0]/1.e3 for y in seis_p]
-	seis_vphi = np.array(s.v_phi())/1.e3#
-	seis_vs = np.array(s.v_s())/1.e3#[seis.seis_V(y/1.e9)[1]/1.e3 for y in seis_p]
-	seis_rho = np.array(s.density())#[seis.seis_density(y/1.e9) for y in seis_p]
-	
-	return seis_p, seis_r, seis_vp, seis_vphi, seis_vs, seis_rho
 
 
 def get_geotherm(name):
