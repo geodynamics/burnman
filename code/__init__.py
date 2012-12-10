11710cee652e9309a9e079c9cb3e0084ee96d2dd
Timo Heister <timo.heister@gmail.com>
2012-12-10 21:49:39 +0000

    work on module init/description
    git-svn-id: https://burnman.googlecode.com/svn/trunk@105 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

---
+++ code/__init__.py
@@ -0,0 +1,14 @@
+"""
+BurnMan
+=======
+
+BurnMan is a lower mantle shear velocity generator constrained by mineral physics.
+
+
+
+"""
+
+from main import *
+import comparison
+import minerals
+import seismic
