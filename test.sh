bfc244dc6725962f07ca6018b8692a69af421a44
Timo Heister <timo.heister@gmail.com>
2012-12-10 23:01:53 +0000

    add missing test
    git-svn-id: https://burnman.googlecode.com/svn/trunk@110 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tests="example_compare_two_models.py example_geotherms.py example_woutput.py example_optimize_pv.py example_composition.py example_seismic.py example_user_input_material.py"
+tests="example_compare_enstpyro.py example_compare_two_models.py example_geotherms.py example_woutput.py example_optimize_pv.py example_composition.py example_seismic.py example_user_input_material.py"
 
 for t in $tests; do
     echo "*** testing '$t' ***"
