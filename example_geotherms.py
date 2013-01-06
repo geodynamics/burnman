50cced1e6b73439013a3cfd80f956ae836766b44
Sanne Cottaar <sanne.cottaar@gmail.com>
2013-01-06 22:09:51 +0000

    license added to codes
    git-svn-id: https://burnman.googlecode.com/svn/trunk@141 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- example_geotherms.py
+++ example_geotherms.py
@@ -1,5 +1,20 @@
-
 """
+    BurnMan- a lower mantle toolkit
+    Copyright (C) 2013, Heister, T., Unterborn, C., Rose, I. and Cottaar, S.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 Shows the various ways to input geotherms: Built-in geothermsi (geotherm1 and 2), basic linear (geotherm3),
 loaded in from a data file (geotherm4) of your choice. Geotherm 1 is from Brown & Shankland (1981) and 
