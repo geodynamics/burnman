01b49719405fd53911699562cf02f303fe0d4a92
Timo Heister <timo.heister@gmail.com>
2013-02-26 21:25:04 +0000

    fix geotherm file, comment broken stuff
    git-svn-id: https://burnman.googlecode.com/svn/trunk@180 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- burnman/geotherm.py
+++ burnman/geotherm.py
@@ -2,11 +2,12 @@
 # Copyright (C) 2012, 2013, Heister, T., Unterborn, C., Rose, I. and Cottaar, S.
 # Released under GPL v2 or later.
 
-import numpy
+import numpy as np
 import bisect
 import matplotlib.pyplot as pyplot
 from tools import *
 import seismic
+import scipy.integrate as integrate
 
 # polynomial fit from Watson, Baxter, EPSL, 2007
 # pressure: in GPa
@@ -27,30 +28,31 @@ def brown_shankland(pressure):
     return lookup_and_interpolate(table_brown_depth, table_brown_temperature, depth)	
 
 #This integrates dT/dP = gr * T / K_s
-def self_consistent(pressure, T0, params):
-        minP = 0
-        #upper mantle potential temperature, in K
-	# integrate the adiabatic gradient equation
-	lnT = integrate.quad( lambda x: (params['ref_grueneisen']/(1.e9*bm.bulk_modulus(x, params))), minP, pressure)
-	T = T0*np.exp(lnT[0])
-	return T
+#TODO: what is this?
+#def self_consistent(pressure, T0, params):
+#    minP = 0
+#    #upper mantle potential temperature, in K
+#    # integrate the adiabatic gradient equation
+#    lnT = integrate.quad( lambda x: (params['ref_grueneisen']/(1.e9*bm.bulk_modulus(x, params))), minP, pressure)
+#    T = T0*np.exp(lnT[0])
+#    return T
     
     
 
 
 table_brown = read_table("data/brown_81.txt")
-table_brown_depth = numpy.array(table_brown)[:,0]
-table_brown_temperature = numpy.array(table_brown)[:,1]
+table_brown_depth = np.array(table_brown)[:,0]
+table_brown_temperature = np.array(table_brown)[:,1]
 
 # test geotherm
 if __name__ == "__main__":
-    p = numpy.arange(1.0,128.0,3)
-    t1 = [geotherm_watsona_baxter(y) for y in p]
-    t2 = [geotherm_brown_shankland(y) for y in p]
-    p2,=pyplot.plot(p,t2,'x--r')
-    p3,=pyplot.plot(p,t3,'*-g')
-    pyplot.xlim(25,135)
-    pyplot.ylim(1600,3100)
+    p = np.arange(1.0e9,128.0e9,3e9)
+    t1 = [watson_baxter(y) for y in p]
+    t2 = [brown_shankland(y) for y in p]
+    p1,=pyplot.plot(p,t1,'x--r')
+    p2,=pyplot.plot(p,t2,'*-g')
+    #pyplot.xlim(25,135)
+    #pyplot.ylim(1600e9,3100e9)
     pyplot.legend([p1,p2],[ "watson", "brown"], loc=4)
 
     pyplot.show()
