b0dacf2793eaba2d0b87b2649b75b668add3fe59
Timo Heister <timo.heister@gmail.com>
2013-02-26 20:55:00 +0000

    update test script
    git-svn-id: https://burnman.googlecode.com/svn/trunk@177 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-tests="example_compare_enstpyro.py example_compare_two_models.py example_geotherms.py example_woutput.py example_optimize_pv.py example_composition.py example_seismic.py example_user_input_material.py"
-
+tests="example_compare_enstpyro.py example_seismic.py example_compare_two_models.py example_spintransition.py example_composition.py example_user_input_material.py example_geotherms.py example_woutput.py example_optimize_pv.py"
 for t in $tests; do
     echo "*** testing '$t' ***"
     python $t || { echo "test $t failed"; exit 1; } 
