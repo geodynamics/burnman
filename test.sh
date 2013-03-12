440e059296c219eabb39c5a4d0a88d8981da3d8c
Timo Heister <timo.heister@gmail.com>
2013-03-12 18:41:56 +0000

    also test table.py
    git-svn-id: https://burnman.googlecode.com/svn/trunk@230 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ python tests.py
 cd ..
 
 
-tests="burnman/benchmark.py example_compare_enstpyro.py example_seismic.py example_compare_two_models.py example_spintransition.py example_composition.py example_user_input_material.py example_geotherms.py example_woutput.py example_optimize_pv.py"
+tests="burnman/benchmark.py example_compare_enstpyro.py example_seismic.py example_compare_two_models.py example_spintransition.py example_composition.py example_user_input_material.py example_geotherms.py example_woutput.py example_optimize_pv.py table.py"
 for t in $tests; do
     echo "*** testing '$t' ***"
     python $t || { echo "test $t failed"; exit 1; } 
