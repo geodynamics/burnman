b798780083d5b533d300cc77acf183ff39a059d1
Timo Heister <timo.heister@gmail.com>
2013-04-20 15:42:34 +0000

    fix tests
    git-svn-id: https://burnman.googlecode.com/svn/trunk@272 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- tests/test_vrh.py
+++ tests/test_vrh.py
@@ -7,19 +7,19 @@ from burnman import minerals, voigt_reuss_hill
 
 class VRH_average(unittest.TestCase):
     def test_one_object(self):
-        v = voigt_reuss_hill.vhr_average([2.0],[0.123])
+        v = voigt_reuss_hill.vrh_average([2.0],[0.123])
         self.assertAlmostEqual(0.123, v)
 
     def test_two_same(self):
-        v = voigt_reuss_hill.vhr_average([2.0, 2.0],[0.456, 0.456])        
+        v = voigt_reuss_hill.vrh_average([2.0, 2.0],[0.456, 0.456])        
         self.assertAlmostEqual(0.456, v)
 
     def test_one_no_volume(self):
-        v = voigt_reuss_hill.vhr_average([0.0, 2.0],[0.123, 0.456])        
+        v = voigt_reuss_hill.vrh_average([0.0, 2.0],[0.123, 0.456])        
         self.assertAlmostEqual(0.456, v)
 
     def test_mix(self):
-        v = voigt_reuss_hill.vhr_average([1.0, 2.0],[0.1, 0.2])        
+        v = voigt_reuss_hill.vrh_average([1.0, 2.0],[0.1, 0.2])        
         self.assertAlmostEqual(0.15833333333333, v)
 
 class VRH(unittest.TestCase):
@@ -29,10 +29,10 @@ class VRH(unittest.TestCase):
         rho, v_p, v_s, v_phi, K_vrh, mu_vrh = \
             voigt_reuss_hill.voigt_reuss_hill(10e9, 300, rock)
         self.assertAlmostEqual(3790.847, rho, 2)
-        self.assertAlmostEqual(10305.374, v_p, 2)
+        self.assertAlmostEqual(10304.13, v_p, 2)
         self.assertAlmostEqual(6315.856, v_s, 2)
-        self.assertAlmostEqual(7281.073, v_phi, 2)
-        self.assertAlmostEqual(200.968, K_vrh/1.e9, 2)
+        self.assertAlmostEqual(7279.312, v_phi, 2)
+        self.assertAlmostEqual(200.870, K_vrh/1.e9, 2)
         self.assertAlmostEqual(151.217, mu_vrh/1.e9, 2)
 
     def same(self, number):
@@ -42,10 +42,10 @@ class VRH(unittest.TestCase):
         rho, v_p, v_s, v_phi, K_vrh, mu_vrh = \
             voigt_reuss_hill.voigt_reuss_hill(10e9, 300, rock)
         self.assertAlmostEqual(3790.847, rho, 2)
-        self.assertAlmostEqual(10305.374, v_p, 2)
+        self.assertAlmostEqual(10304.130, v_p, 2)
         self.assertAlmostEqual(6315.856, v_s, 2)
-        self.assertAlmostEqual(7281.073, v_phi, 2)
-        self.assertAlmostEqual(200.968, K_vrh/1.e9, 2)
+        self.assertAlmostEqual(7279.312, v_phi, 2)
+        self.assertAlmostEqual(200.870, K_vrh/1.e9, 2)
         self.assertAlmostEqual(151.217, mu_vrh/1.e9, 2)
 
     def test_same(self):
@@ -60,10 +60,10 @@ class VRH(unittest.TestCase):
         rho, v_p, v_s, v_phi, K_vrh, mu_vrh = \
             voigt_reuss_hill.voigt_reuss_hill(10e9, 300, rock)
         self.assertAlmostEqual(3790.847, rho, 2)
-        self.assertAlmostEqual(10305.374, v_p, 2)
+        self.assertAlmostEqual(10304.130, v_p, 2)
         self.assertAlmostEqual(6315.856, v_s, 2)
-        self.assertAlmostEqual(7281.073, v_phi, 2)
-        self.assertAlmostEqual(200.968, K_vrh/1.e9, 2)
+        self.assertAlmostEqual(7279.312, v_phi, 2)
+        self.assertAlmostEqual(200.870, K_vrh/1.e9, 2)
         self.assertAlmostEqual(151.217, mu_vrh/1.e9, 2)
 
 
