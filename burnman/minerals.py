c41bba8dfdf8fa4759a5cd5b05c9eef49a8a8cd2
Sanne Cottaar <sanne.cottaar@gmail.com>
2013-04-22 20:50:22 +0000

    read in slb 2011
    git-svn-id: https://burnman.googlecode.com/svn/trunk@393 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- burnman/minerals.py
+++ burnman/minerals.py
@@ -2,6 +2,8 @@
 # Copyright (C) 2012, 2013, Heister, T., Unterborn, C., Rose, I. and Cottaar, S.
 # Released under GPL v2 or later.
 
+
+from minlib_SLB2011 import *
 from minlib_SLB2005 import *
 from minlib_Matetal2007 import *
 from minlib_Muretal2012  import *
