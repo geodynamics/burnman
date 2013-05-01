2c1ea84838d4e2b2f4eb2cada124b9cf24362904
Timo Heister <timo.heister@gmail.com>
2013-05-01 23:42:06 +0000

    update release script
    git-svn-id: https://burnman.googlecode.com/svn/trunk@437 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- release.sh
+++ release.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-zip -v release.zip *py readme.txt burnman/*py data/*.txt
+
+name="burnman-0.3"
+
+rm -rf $name
+
+svn export https://burnman.googlecode.com/svn/trunk $name
+rm -rf $name/misc/ $name/todo.txt $name/release.sh $name/test.sh play_withslb.py reproduce_*.py
+
+
+
+zip -v $name.zip *py readme.txt burnman/*py data/*.txt
