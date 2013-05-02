ef004f642a2bbd852c57f1fceed4f44d6b2acca4
Timo Heister <timo.heister@gmail.com>
2013-05-02 00:17:56 +0000

    fix release script
    git-svn-id: https://burnman.googlecode.com/svn/trunk@439 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- release.sh
+++ release.sh
@@ -6,8 +6,8 @@ name="burnman-v0.3"
 rm -rf $name
 
 svn export https://burnman.googlecode.com/svn/trunk $name
-rm -rf $name/misc/ $name/todo.txt $name/release.sh $name/test.sh play_withslb.py $name/reproduce_*.py $name/play_withslb.py
+rm -rf $name/misc/ $name/todo.txt $name/release.sh $name/test.sh $name/reproduce_*.py $name/play_withslb.py
 
 
 
-zip -v $name.zip *py readme.txt burnman/*py data/*.txt
+zip -v $name.zip $name
