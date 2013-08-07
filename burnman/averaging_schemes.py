Fix bug in density averaging...we will need to fix the test suite as well.
git-svn-id: https://burnman.googlecode.com/svn/trunk@501 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

burnman/averaging_schemes.py
@@ -28,13 +28,15 @@ class averaging_scheme:
         raise NotImplementedError("")
     def average_density(self, volumes, densities):
         """
-        Average the densities of the rock.  This is the only
+        Average the densities of the rock, given a list of volume
+        fractions and densitites.  This is the only
         one that is implemented in the base class, as it should
         not be controvsersial... :)
         Returns: a single density
         """
         total_mass = np.sum(np.array(densities)*np.array(volumes))
-        total_vol = np.sum(np.array(volumes))
+        total_vol = np.sum(np.array(volumes)) #should sum to one
+        assert(total_vol < 1.0001 and total_vol > 0.9999)
         density = total_mass/total_vol
         return density
          

burnman/composite.py
@@ -54,7 +54,8 @@ class composite:
         Compute the density of the composite based on the molar volumes and masses
         """
         densities = np.array([ph.mineral.density() for ph in self.phases])
-        volumes = np.array([ph.mineral.molar_volume() for ph in self.phases])
+        volumes = np.array([ph.mineral.molar_volume() for ph in self.phases])\
+                           *np.array([ph.fraction for ph in self.phases])
         return np.sum(densities*volumes)/np.sum(volumes)
         
                 

burnman/main.py
@@ -74,15 +74,20 @@ def average_moduli(moduli_list, averaging_scheme=averaging_schemes.voigt_reuss_h
     
     for idx in range(n_pressures):
         fractions = [m.fraction[idx] for m in moduli_list]
+
+        #come up with volume fractions of the phases
         V_ph = [m.V[idx] for m in moduli_list]
+        V_mol = np.array(V_ph)*np.array(fractions)
+        V_frac = V_mol/sum(V_mol)
+
         K_ph = [m.K[idx] for m in moduli_list]
         G_ph = [m.G[idx] for m in moduli_list]
         rho_ph = [m.rho[idx] for m in moduli_list]
                
-        result.V[idx] = sum(V_ph)
-        result.K[idx] = averaging_scheme.average_bulk_moduli(V_ph, K_ph, G_ph)
-        result.G[idx] = averaging_scheme.average_shear_moduli(V_ph, K_ph, G_ph)
-        result.rho[idx] = averaging_scheme.average_density(V_ph, rho_ph)
+        result.V[idx] = sum(V_mol)
+        result.K[idx] = averaging_scheme.average_bulk_moduli(V_frac, K_ph, G_ph)
+        result.G[idx] = averaging_scheme.average_shear_moduli(V_frac, K_ph, G_ph)
+        result.rho[idx] = averaging_scheme.average_density(V_frac, rho_ph)
         result.fraction[idx] = 1.0
 
     return result