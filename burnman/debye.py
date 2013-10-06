a1cb1cbcec434dd48049d9d433de8836eb9a4eaa
Ian Rose <ian.rose@berkeley.edu>
2013-10-06 23:19:18 +0000

    Change how the integrals are evaluated (now use chebyshev/matched asymptotics), which results in a large speedup
    git-svn-id: https://burnman.googlecode.com/svn/trunk@542 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- burnman/debye.py
+++ burnman/debye.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 import scipy.integrate as integrate
+import matplotlib.pyplot as plt
 
 """
 Functions for the Debye model.  Note that this is not Mie-Grueneisen-Debye, 
@@ -13,6 +14,8 @@ Birch-Murnaghan to get a full EOS
 
 R = 8.314462175
 
+
+
 def debye_fn(x):
     """
     Evaluate the Debye function.  Takes the parameter
@@ -20,6 +23,57 @@ def debye_fn(x):
     """
     sol = integrate.quad( lambda xi: pow(xi,3.)/(np.exp(xi)-1.) , 0.0, x) # EQ B3
     return 3.*sol[0]/pow(x,3.)
+
+
+
+from numpy.polynomial.chebyshev import Chebyshev
+chebyshev_representation = Chebyshev( [ 2.707737068327440945/2.0, 0.340068135211091751, -0.12945150184440869e-01, \
+                                     0.7963755380173816e-03, -0.546360009590824e-04, 0.39243019598805e-05, \
+                                    -0.2894032823539e-06, 0.217317613962e-07, -0.16542099950e-08, \
+                                     0.1272796189e-09, -0.987963460e-11, 0.7725074e-12, -0.607797e-13, \
+                                     0.48076e-14, -0.3820e-15, 0.305e-16, -0.24e-17] )
+eps = np.finfo(np.float).eps
+sqrt_eps = np.sqrt(np.finfo(np.float).eps)
+log_eps = np.log(np.finfo(np.float).eps)
+
+def debye_fn_gsl(x):
+    """
+    Evaluate the Debye function using a Chebyshev series expansion coupled with 
+    asymptotic solutions of the function.  Shamelessly adapted from the GSL implementation
+    of the same function.  Should give the same result as debye_fn(x) to near
+    machine-precision.
+    """
+    val_infinity = 19.4818182068004875;
+    xcut = -log_eps
+
+    assert(x > 0.0) #check for invalid x
+
+    if x < 2.0*np.sqrt(2.0)*sqrt_eps:
+        return 1.0 - 3.0*x/8.0 + x*x/20.0;
+    elif x <= 4.0 :
+        t = x*x/8.0 - 1.0;
+        c = chebyshev_representation(t)
+        return c - 0.375*x;
+    elif x < -(np.log(2.0) + log_eps ):
+        nexp = int(np.floor(xcut/x));
+        ex  = np.exp(-x);
+        xk  = nexp * x;
+        rk  = nexp;
+        sum = 0.0;
+        for i in range(nexp,0,-1):
+            xk_inv = 1.0/xk;
+            sum *= ex;
+            sum += (((6.0*xk_inv + 6.0)*xk_inv + 3.0)*xk_inv + 1.0) / rk;
+            rk -= 1.0;
+            xk -= x;
+        return val_infinity/(x*x*x) - 3.0 * sum * ex;
+    elif x < xcut:
+        x3 = x*x*x;
+        sum = 6.0 + 6.0*x + 3.0*x*x + x3;
+        return  (val_infinity - 3.0 * sum * np.exp(-x)) / x3;
+    else:
+        return ((val_infinity/x)/x)/x;
+
 
 def thermal_energy(T, debye_T, n):
     """
@@ -29,16 +83,36 @@ def thermal_energy(T, debye_T, n):
     """
     if T == 0:
         return 0
-    E_th = 3.*n*R*T * debye_fn(debye_T/T)
+    E_th = 3.*n*R*T * debye_fn_gsl(debye_T/T)
     return E_th
 
-def heat_capacity_v(T, debye_T, n):
+def heat_capacity_v(T,debye_T,n):
     """
     Heat capacity at constant volume.  In J/K/mol
     """
     if T ==0:
         return 0
-    deb = integrate.quad( lambda x : pow(x,4.)*np.exp(x)/pow((np.exp(x)-1.),2.), 0.0, debye_T/T)
-    C_v = 9.*n*R*deb[0]/pow(debye_T/T,3.)
+    x = debye_T/T
+    C_v = 3.0*n*R* ( 4.0*debye_fn_gsl(x) - 3.0*x/(np.exp(x)-1.0) )
     return C_v
 
+
+
+
+if __name__ == "__main__":
+    temperatures = np.linspace(0,5000, 200)
+    vibrational_energy = np.empty_like(temperatures)
+    heat_capacity = np.empty_like(temperatures)
+    Debye_T = 1000.
+    for i in range(len(temperatures)):
+      vibrational_energy[i] = thermal_energy(temperatures[i], Debye_T, 1.0)
+      heat_capacity[i] = heat_capacity_v(temperatures[i], Debye_T, 1.0)
+
+    plt.subplot(121)
+    plt.plot(temperatures, vibrational_energy)
+    plt.subplot(122)
+    plt.plot(temperatures, heat_capacity)
+    plt.show()
+
+
+    
