377d94b041bb9d2df23c09db1c4f2ec82743870d
Timo Heister <timo.heister@gmail.com>
2013-10-06 16:28:48 +0000

    make example run
    git-svn-id: https://burnman.googlecode.com/svn/trunk@541 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- example_uncertainty.py
+++ example_uncertainty.py
@@ -13,7 +13,7 @@ if not os.path.exists('burnman') and os.path.exists('../burnman'):
 
 import burnman
 from burnman import minerals
-import colors
+import misc.colors as colors 
 
 
 if __name__ == "__main__":    
