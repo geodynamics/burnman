e86bef08ca471c23a7a0e3799dc5258ba7845936
Ian Rose <ian.rose@berkeley.edu>
2014-01-30 18:25:11 +0000

    Reproducing figure in book chapter...
    git-svn-id: https://burnman.googlecode.com/svn/trunk@600 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

---
+++ murakami_book_chapter.py
@@ -0,0 +1,181 @@
+# BurnMan - a lower mantle toolkit
+# Copyright (C) 2012, 2013, Heister, T., Unterborn, C., Rose, I. and Cottaar, S.
+# Released under GPL v2 or later.
+
+"""
+Attempt to reproduce Figure 6.12 From chapter 6 of Physics and Chemistry of the Deep Earth, 2013
+Book chapter by Motohiko Murakami, editor Shun-ichiro Karato
+"""
+
+import os, sys, numpy as np, matplotlib.pyplot as plt
+#hack to allow scripts to be placed in subdirectories next to burnman:
+if not os.path.exists('burnman') and os.path.exists('../burnman'):
+    sys.path.insert(1,os.path.abspath('..')) 
+
+import burnman
+from burnman import minerals
+import matplotlib.image as mpimg
+import burnman.minerals_base as mb
+import numpy as np
+
+    
+method = 'slb2'
+    
+#define the minerals from table 6.3
+mg_perovskite = burnman.material()
+mg_perovskite.params = {       'name': 'Mg perovskite',
+                    'molar_mass' : 0.1004,
+                    'V_0': 24.43e-6,
+                    'K_0': 253.0e9,
+                    'Kprime_0': 3.9,
+                    'G_0' : 172.9e9,
+                    'Gprime_0' : 1.56,
+                    'n': 5.0,
+                    'Debye_0': 1100.,
+                    'grueneisen_0': 1.40,
+                    'q_0': 1.40, 
+                    'eta_s_0' : 2.6}
+mg_perovskite.set_method('slb2')
+
+fe_perovskite = burnman.material()
+fe_perovskite.params = {       'name': 'Fe perovskite',
+                    'molar_mass' : 0.1319,
+                    'V_0': 25.49e-6,
+                    'K_0': 281.0e9,
+                    'Kprime_0': 4.1,
+                    'G_0' : 138.0e9,
+                    'Gprime_0' : 1.70,
+                    'n': 5.0,
+                    'Debye_0': 841.,
+                    'grueneisen_0': 1.48,
+                    'q_0': 1.40, 
+                    'eta_s_0' : 2.1}
+fe_perovskite.set_method(method)
+
+periclase = burnman.material()
+periclase.params = {       'name': 'periclase',
+                    'molar_mass' : 0.0403,
+                    'V_0': 11.24e-6,
+                    'K_0': 161.0e9,
+                    'Kprime_0': 3.9,
+                    'G_0' : 130.9e9,
+                    'Gprime_0' : 1.92,
+                    'n': 2.0,
+                    'Debye_0': 773.,
+                    'grueneisen_0': 1.50,
+                    'q_0': 1.50, 
+                    'eta_s_0' : 2.3}
+periclase.set_method(method)
+
+wustite = burnman.material()
+wustite.params = {       'name': 'wustite',
+                    'molar_mass' : 0.07184,
+                    'V_0': 12.06e-6,
+                    'K_0': 152.0e9,
+                    'Kprime_0': 4.9,
+                    'G_0' : 47.0e9,
+                    'Gprime_0' : 0.70,
+                    'n': 2.0,
+                    'Debye_0': 455.,
+                    'grueneisen_0': 1.28,
+                    'q_0': 1.50, 
+                    'eta_s_0' : 0.8}
+wustite.set_method(method)
+
+
+#in the text for the book chapter a linear relationship in elastic properties
+#for the solid solutions is assumed...
+class ferropericlase(mb.helper_solid_solution):
+    def __init__(self, fe_num):
+        base_materials = [periclase, wustite]
+        molar_fraction = [1. - fe_num, 0.0 + fe_num]
+        mb.helper_solid_solution.__init__(self, base_materials, molar_fraction)
+
+
+
+class perovskite(mb.helper_solid_solution):
+    def __init__(self, fe_num):
+        base_materials = [mg_perovskite, fe_perovskite]
+        molar_fraction = [1. - fe_num, 0.0 + fe_num]
+        mb.helper_solid_solution.__init__(self, base_materials, molar_fraction)
+
+
+#define the P-T path
+pressure = np.linspace(28.0e9, 129e9, 25.)
+temperature_bs = burnman.geotherm.brown_shankland(pressure)
+temperature_an = burnman.geotherm.anderson(pressure)
+
+#seismic model for comparison:
+seismic_model = burnman.seismic.prem() # pick from .prem() .slow() .fast() (see burnman/seismic.py)
+depths = map(seismic_model.depth, pressure)
+seis_p, seis_rho, seis_vp, seis_vs, seis_vphi = seismic_model.evaluate_all_at(depths)    
+
+#pure perovskite
+perovskitite = burnman.composite( ( (perovskite(0.06), 1.0),) )
+perovskitite.set_method(method)
+ 
+#pure periclase
+periclasite = burnman.composite( ( (ferropericlase(0.21), 1.0),))
+periclasite.set_method(method) 
+
+#pyrolite (80% perovskite)
+pyrolite = burnman.composite( ( (perovskite(0.06), 0.80),
+                              (ferropericlase(0.21), 0.20) ) )
+pyrolite.set_method(method) 
+
+#preferred mixture?
+amount_perovskite = 0.92
+preferred_mixture = burnman.composite( ( (perovskite(0.06), amount_perovskite),
+                                         (ferropericlase(0.21), 1.0-amount_perovskite) ) )
+preferred_mixture.set_method(method) 
+    
+
+mat_rho_1, mat_vp_1, mat_vs_1, mat_vphi_1, mat_K_1, mat_G_1 = burnman.velocities_from_rock(perovskitite,seis_p, temperature_bs)    
+mat_rho_2, mat_vp_2, mat_vs_2, mat_vphi_2, mat_K_2, mat_G_2 = burnman.velocities_from_rock(periclasite,seis_p, temperature_bs)    
+mat_rho_3, mat_vp_3, mat_vs_3, mat_vphi_3, mat_K_3, mat_G_3 = burnman.velocities_from_rock(pyrolite,seis_p, temperature_bs)
+mat_rho_4, mat_vp_4, mat_vs_4, mat_vphi_4, mat_K_4, mat_G_4 = burnman.velocities_from_rock(preferred_mixture,seis_p, temperature_bs)
+
+
+
+### HERE IS THE STEP WITH THE INCORRECT MIXING ###
+# comment this out to have correct phase averaging, leave it in to have incorrect phase averaging
+
+#mat_vs_3 = 0.5*((0.80*mat_vs_1 + 0.20*mat_vs_2) + np.ones_like(mat_vs_1)/(0.80/mat_vs_1 + 0.20/mat_vs_2))
+#mat_vs_4 = 0.5*((0.92*mat_vs_1 + 0.08*mat_vs_2) + np.ones_like(mat_vs_1)/(0.92/mat_vs_1 + 0.08/mat_vs_2))
+
+plt.subplot(1,1,1)
+plt.ylim(5,7.6) 
+plt.xlim(25,135)
+fig1 = mpimg.imread('input_figures/murakami_book_chapter.png')
+plt.imshow(fig1, extent=[25,135,5.0,7.6], aspect='auto')
+plt.plot(seis_p/1.e9,mat_vs_1/1.e3,color='b',linestyle='--',marker='o',markerfacecolor='b',markersize=3)
+plt.plot(seis_p/1.e9,mat_vs_2/1.e3,color='k',linestyle='--',marker='o',markerfacecolor='k',markersize=3)
+plt.plot(seis_p/1.e9,mat_vs_3/1.e3,color='g',linestyle='--',marker='o',markerfacecolor='g',markersize=3)
+plt.plot(seis_p/1.e9,mat_vs_4/1.e3,color='r',linestyle='--',marker='o',markerfacecolor='r',markersize=3)
+plt.plot(seis_p/1.e9,seis_vs/1.e3,color='k',linestyle='',marker='o',markerfacecolor='w',markersize=4)
+plt.title("Vs (km/s), VRH on moduli")
+plt.savefig("reproduce_murakami_moduli_a.pdf")
+plt.show()
+
+mat_rho_1, mat_vp_1, mat_vs_1, mat_vphi_1, mat_K_1, mat_G_1 = burnman.velocities_from_rock(perovskitite,seis_p, temperature_an)    
+mat_rho_2, mat_vp_2, mat_vs_2, mat_vphi_2, mat_K_2, mat_G_2 = burnman.velocities_from_rock(periclasite,seis_p, temperature_an)    
+mat_rho_3, mat_vp_3, mat_vs_3, mat_vphi_3, mat_K_3, mat_G_3 = burnman.velocities_from_rock(pyrolite,seis_p, temperature_an)
+
+
+# again, leave this line in to have incorrect phase averaging...
+#mat_vs_3 = 0.5*((0.80*mat_vs_1 + 0.20*mat_vs_2) + np.ones_like(mat_vs_1)/(0.80/mat_vs_1 + 0.20/mat_vs_2))
+
+plt.subplot(1,1,1)
+plt.ylim(5,7.6) 
+plt.xlim(25,135)
+fig2 = mpimg.imread('input_figures/murakami_book_chapter_b.png')
+plt.imshow(fig2, extent=[25,135,5.0,7.6], aspect='auto')
+plt.plot(seis_p/1.e9,seis_vs/1.e3,color='k',linestyle='',marker='o',markerfacecolor='w',markersize=4)
+plt.plot(seis_p/1.e9,mat_vs_1/1.e3,color='b',linestyle='--',marker='o',markerfacecolor='b',markersize=3)
+plt.plot(seis_p/1.e9,mat_vs_2/1.e3,color='k',linestyle='--',marker='o',markerfacecolor='k',markersize=3)
+plt.plot(seis_p/1.e9,mat_vs_3/1.e3,color='g',linestyle='--',marker='o',markerfacecolor='g',markersize=3)
+plt.title("Vs (km/s), VRH on moduli")
+plt.savefig("reproduce_murakami_moduli_b.pdf")
+plt.show()
+
+
