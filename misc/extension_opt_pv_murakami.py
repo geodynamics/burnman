d88c0d56fe2920301bed8d694b0d1424b7b487e0
Sanne Cottaar <sanne.cottaar@gmail.com>
2014-02-03 21:18:19 +0000

    opt_pv for murakami publications
    git-svn-id: https://burnman.googlecode.com/svn/trunk@602 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

---
+++ misc/extension_opt_pv_murakami.py
@@ -0,0 +1,197 @@
+# BurnMan - a lower mantle toolkit
+# Copyright (C) 2012, 2013, Heister, T., Unterborn, C., Rose, I. and Cottaar, S.
+# Released under GPL v2 or later.
+
+"""
+Vary the amount perovskite vs. ferropericlase and compute the error in the
+seismic data against PREM.
+
+requires:
+- creating minerals
+- compute seismic velocities
+- geotherms
+- seismic models
+- seismic comparison
+
+teaches:
+- compare errors between models
+- loops over models
+
+"""
+
+import os, sys, numpy as np, matplotlib.pyplot as plt
+#hack to allow scripts to be placed in subdirectories next to burnman:
+if not os.path.exists('burnman') and os.path.exists('../burnman'):
+    sys.path.insert(1,os.path.abspath('..')) 
+
+import burnman
+from burnman import minerals
+import colors
+
+if __name__ == "__main__":    
+
+    #figsize=(6,5)
+    plt.figure(dpi=100,figsize=(12,10))
+    prop={'size':12}
+    plt.rc('text', usetex=True)
+    plt.rcParams['text.latex.preamble'] = '\usepackage{relsize}'
+#    plt.rc('font', family='sanserif')
+    figsize=(6,5)
+
+    dashstyle2=(7,3)
+    dashstyle3=(3,2)
+    
+    ### input variables ###
+    #######################
+    
+    #INPUT for method
+    """ choose 'slb2' (finite-strain 2nd order shear modulus,       
+        stixrude and lithgow-bertelloni, 2005)
+    or 'slb3 (finite-strain 3rd order shear modulus,
+        stixrude and lithgow-bertelloni, 2005)
+    or 'mgd3' (mie-gruneisen-debeye 3rd order shear modulus,
+        matas et al. 2007)
+    or 'mgd2' (mie-gruneisen-debeye 2nd order shear modulus,
+        matas et al. 2007)
+    or 'bm2' (birch-murnaghan 2nd order, if you choose to ignore temperature
+       (your choice in geotherm will not matter in this case))
+    or 'bm3' (birch-murnaghan 3rd order, if you choose to ignore temperature
+        (your choice in geotherm will not matter in this case))"""
+    method = 'slb2'
+    
+    seismic_model = burnman.seismic.prem() # pick from .prem() .slow() .fast() (see burnman/seismic.py)
+    number_of_points = 20 #set on how many depth slices the computations should be done
+    depths = np.linspace(850e3,2700e3, number_of_points)
+    seis_p, seis_rho, seis_vp, seis_vs, seis_vphi = seismic_model.evaluate_all_at(depths)
+
+    print seis_p[0], seis_p[-1]
+    
+    temperature = burnman.geotherm.brown_shankland(seis_p)
+    
+    def eval_material(amount_perovskite):
+        rock = burnman.composite ( [ (minerals.Murakami_etal_2012.fe_perovskite(), amount_perovskite),
+                                    (minerals.Murakami_etal_2012.fe_periclase(), 1.0 - amount_perovskite) ] )
+        #        rock = burnman.composite ( [ (minerals.Murakami_2013.mg_fe_perovskite(0.06), amount_perovskite),
+        #                    (minerals.Murakami_2013.ferropericlase(0.21), 1.0 - amount_perovskite) ] )
+#        rock = burnman.composite ( [ (minerals.SLB_2011.mg_fe_perovskite(0.), amount_perovskite),
+#                             (minerals.SLB_2011.ferropericlase(1.0), 1.0 - amount_perovskite) ] )
+        rock.set_method(method)
+        #temperature =  burnman.geotherm.adiabatic(seis_p,1900,rock)
+        print "Calculations are done for:"
+        rock.debug_print()
+    
+        mat_rho, mat_vp, mat_vs, mat_vphi, mat_K, mat_G = \
+            burnman.velocities_from_rock(rock, seis_p, temperature, burnman.averaging_schemes.voigt_reuss_hill())
+    
+        #[rho_err,vphi_err,vs_err]=burnman.compare_chifactor(mat_vs,mat_vphi,mat_rho,seis_vs,seis_vphi,seis_rho)
+        
+    
+        return seis_p, mat_vs, mat_vphi, mat_rho
+
+    def material_error(x):
+        _, mat_vs, mat_vphi, mat_rho = eval_material(x)
+        [rho_err,vphi_err,vs_err]=burnman.compare_l2(depths,mat_vs,mat_vphi,mat_rho,seis_vs,seis_vphi,seis_rho)
+        scale = 2700e3-850e3
+        return vs_err/scale, vphi_err/scale
+
+    xx=np.linspace(0.0, 1.0, 200) #200 for final image
+    errs=np.array([material_error(x) for x in xx])
+    yy_vs=errs[:,0]
+    yy_vphi=errs[:,1]
+    vs_average_prem = sum(seis_vs)/len(seis_vs)
+    vphi_average_prem = sum(seis_vphi)/len(seis_vphi)
+    print vs_average_prem, vphi_average_prem
+    yy_vs=yy_vs/vs_average_prem
+    yy_vphi=yy_vphi/vphi_average_prem
+    yy_sum = (yy_vs+yy_vphi) #we scale by a factor so it fits in the plot 
+ #   plt.figure(dpi=100,figsize=figsize)
+    plt.subplot(2,2,1)
+    plt.plot (xx*100,yy_vs,"-",color=colors.color(1),label=("$V_s$ error"),linewidth=1.5,dashes=dashstyle2)
+    plt.plot (xx*100,yy_vphi,"-",color=colors.color(3),label=("$V_\phi$ error"),linewidth=1.5)
+    #plt.plot (xx*100,yy_vs+yy_vphi,"g--",label=("sum"),linewidth=1.5)
+    plt.plot (xx*100,yy_sum,"-",color=colors.color(4),label=("weighted sum"),linewidth=1.5,dashes=dashstyle3)
+
+    ymin = 1e-2
+    ymax = 1e2
+    plt.ylim([ymin,ymax])
+
+    print xx[np.argmin(yy_vs)], xx[np.argmin(yy_vphi)], xx[np.argmin(yy_sum)]
+
+    B = np.around(xx[np.argmin(yy_vs)], decimals=3)
+    A = np.around(xx[np.argmin(yy_vphi)], decimals=3)
+    C = np.around(xx[np.argmin(yy_sum)], decimals=3)
+
+    plt.plot([A*100.,A*100.],[ymin,ymax],color=colors.color(3),label='A (%g\%% pv)'%(A*100),linewidth=1.5,linestyle='-')
+    plt.plot([B*100.,B*100.],[ymin,ymax],color=colors.color(1),label='B (%g\%% pv)'%(B*100),linewidth=1.5,dashes=dashstyle2)
+    plt.plot([C*100.,C*100.],[ymin,ymax],color=colors.color(4),label='C (%g\%% pv)'%(C*100),linewidth=1.5,dashes=dashstyle3)
+    
+    plt.yscale('log')
+    plt.xlabel('\% Perovskite')
+    plt.ylabel('Error')
+    plt.legend(loc='lower left',prop=prop)
+#    plt.tight_layout(pad=2)
+#    plt.savefig("opt_pv_1.pdf",bbox_inches='tight')
+#    plt.show()
+
+    A_p, A_vs, A_vphi,_ = eval_material(A)
+    B_p, B_vs, B_vphi,_ = eval_material(B)
+    C_p, C_vs, C_vphi,_ = eval_material(C)
+    
+#    plt.figure(dpi=100,figsize=figsize)
+    plt.subplot(2,2,3)
+    plt.plot(seis_p/1.e9,seis_vs/1.e3,color='k',linestyle='-',linewidth=2.0, markersize=6,markerfacecolor='None',label='PREM')
+    plt.plot(A_p/1.e9,A_vs/1.e3,color=colors.color(3),linestyle='-', \
+    label='A (%g\%% pv)'%(A*100),linewidth=1.5,markevery=5,marker='v',markerfacecolor='None',mew=1.5, markeredgecolor=colors.color(3))
+    plt.plot(B_p/1.e9,B_vs/1.e3,color=colors.color(1),dashes=dashstyle2, \
+    label='B (%g\%% pv)'%(B*100),linewidth=1.5,markevery=5,marker='v',markerfacecolor='None',mew=1.5, markeredgecolor=colors.color(1))
+    plt.plot(C_p/1.e9,C_vs/1.e3,color=colors.color(4),dashes=dashstyle3, \
+    label='C (%g\%% pv)'%(C*100),linewidth=1.5,markevery=5,marker='v',markerfacecolor='None',mew=1.5, markeredgecolor=colors.color(4))
+    plt.xlabel('Pressure (GPa)')
+    plt.ylabel('Shear velocity $V_{\mathlarger{\mathlarger{\mathlarger{s}}}}$ (km/s)')
+    plt.xlim([30,130])
+    plt.legend(loc='lower right',prop=prop)
+  #  plt.tight_layout()
+ #   plt.savefig("opt_pv_2.pdf",bbox_inches='tight')
+#    plt.show()
+   
+    plt.subplot(2,2,4)
+   
+#    plt.figure(dpi=100,figsize=figsize)
+    plt.plot(seis_p/1.e9,seis_vphi/1.e3,color='k',linestyle='-',linewidth=2.0, markersize=6,markerfacecolor='None',label='PREM', mew=1.5)
+    plt.plot(A_p/1.e9,A_vphi/1.e3,color=colors.color(3),linestyle='-', \
+                 markevery=5,marker='s',markersize=5,markeredgecolor=colors.color(3),markerfacecolor='None',mew=1.5,label='A (%g\%% pv)'%(A*100),linewidth=1.5)
+    plt.plot(B_p/1.e9,B_vphi/1.e3,color=colors.color(1),dashes=dashstyle2, \
+                 markevery=5,marker='s',markersize=5,markeredgecolor=colors.color(1),markerfacecolor='None',mew=1.5,label='B (%g\%% pv)'%(B*100),linewidth=1.5)
+    plt.plot(C_p/1.e9,C_vphi/1.e3,color=colors.color(4),dashes=dashstyle3, \
+                 markevery=5,marker='s',markersize=5,markeredgecolor=colors.color(4),markerfacecolor='None',mew=1.5,label='C (%g\%% pv)'%(C*100),linewidth=1.5)
+    plt.xlabel('Pressure (GPa)')
+    plt.ylabel("Bulk sound velocity $V_{\mathlarger{\mathlarger{\mathlarger{\phi}}}}$ (km/s)")
+    plt.xlim([30,130])
+    plt.legend(loc='lower right', prop=prop)
+    #plt.tight_layout()
+    #plt.savefig("opt_pv_3.pdf",bbox_inches='tight')
+    #plt.show()
+
+
+    # plot percent differences
+#    plt.figure(dpi=100,figsize=figsize)
+    plt.subplot(2,2,2)
+    plt.plot(seis_p/1.e9, seis_vs*0.0,color='k',linestyle='-',linewidth=2.0)
+    plt.plot(seis_p/1.e9, (A_vs-seis_vs)/seis_vs*100.0,color=colors.color(3),label='$V_s$: A (%g\%% pv)'%(A*100),linewidth=1.5,linestyle='-',markevery=5,marker='v',markerfacecolor='None',mew=1.5, markeredgecolor=colors.color(3))
+    plt.plot(seis_p/1.e9, (B_vs-seis_vs)/seis_vs*100.0,color=colors.color(1),label='$V_s$: B (%g\%% pv)'%(B*100),linewidth=1.5,dashes=dashstyle2,markevery=5,marker='v',markerfacecolor='None',mew=1.5, markeredgecolor=colors.color(1))
+    plt.plot(seis_p/1.e9, (C_vs-seis_vs)/seis_vs*100.0,color=colors.color(4),label='$V_s$: C (%g\%% pv)'%(C*100),linewidth=1.5,dashes=dashstyle3,markevery=5,marker='v',markerfacecolor='None',mew=1.5, markeredgecolor=colors.color(4))
+    plt.plot(seis_p/1.e9, (A_vphi-seis_vphi)/seis_vphi*100.0,color=colors.color(3),markevery=5,marker='s',markersize=5,markeredgecolor=colors.color(3),markerfacecolor='None',mew=1.5,label='$V_\phi$: A',linewidth=1.5,linestyle='-')
+    plt.plot(seis_p/1.e9, (B_vphi-seis_vphi)/seis_vphi*100.0,color=colors.color(1),markevery=5,marker='s',markersize=5,markeredgecolor=colors.color(1),markerfacecolor='None',mew=1.5,label='$V_\phi$: B',linewidth=1.5,dashes=dashstyle2)
+    plt.plot(seis_p/1.e9, (C_vphi-seis_vphi)/seis_vphi*100.0,color=colors.color(4),markevery=5,marker='s',markersize=5,markeredgecolor=colors.color(4),markerfacecolor='None',mew=1.5,label='$V_\phi$: C',linewidth=1.5,dashes=dashstyle3)
+    plt.xlabel('Pressure (GPa)')
+    plt.ylabel('Difference from PREM (\%)')
+    plt.ylim([-5,4])
+    plt.xlim([30,130]) 
+    plt.legend(loc='lower center', ncol=2, prop=prop)
+#    plt.tight_layout()
+#    plt.savefig("opt_pv_4.pdf",bbox_inches='tight')
+#    plt.show()
+
+#    plt.tight_layout()
+    plt.savefig("extension_opt_pv_murakami.pdf",bbox_inches='tight')
+    plt.show()
