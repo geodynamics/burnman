cfe061dc8b05836cf7f049183be44ead89657810
Timo Heister <timo.heister@gmail.com>
2014-05-28 21:19:02 +0000

    turn on source code linking
    git-svn-id: https://burnman.googlecode.com/svn/trunk@647 7ecf5a11-160e-3e22-d4cb-c1a8d750ac10

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -28,7 +28,7 @@ import burnman
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.ifconfig', 'sphinx.ext.inheritance_diagram']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.ifconfig', 'sphinx.ext.inheritance_diagram','sphinx.ext.viewcode']
 
 autodoc_default_flags = ['members', 'undoc-members']
 
