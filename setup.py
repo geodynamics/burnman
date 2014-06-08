cba145709996de0136f6419a3d2c6b7dd4bc55d6
Timo Heister <timo.heister@gmail.com>
2014-06-08 09:42:05 -0400

    installer updates
    switch to setuptools from distutils, update description

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup ( name = 'burnman',
-        version = '0.6',
+        version = '0.6b3',
+        description='a lower mantle toolkit',
+        url='http://burnman.org',
+        author='Ian Rose',
+        author_email='ian.rose@berkeley.edu',
+        license='GPL',
+        long_description='BurnMan is a lower mantle shear velocity generator constrained by mineral physics.',
         packages = ['burnman', 'burnman.minerals'],
-        package_data = { 'burnman' : ['data/input_*/*'] }
+        package_data = { 'burnman' : ['data/input_*/*'] },
+        install_requires=['numpy', 'matplotlib', 'scipy']
        )
