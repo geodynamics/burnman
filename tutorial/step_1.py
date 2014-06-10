817a0633b89a32178b43ea58346f6bf324e2d1c2
ian-r-rose <ian.r.rose@gmail.com>
2014-06-10 13:21:47 -0700

    Add initial versions of tutorials

---
+++ tutorial/step_1.py
@@ -0,0 +1,106 @@
+# BurnMan - a lower mantle toolkit
+# Copyright (C) 2014, Heister, T., Unterborn, C., Rose, I. and Cottaar, S.
+# Released under GPL v2 or later.
+
+"""
+"""
+
+# Here we import standard python modules that are required for
+# usage of BurnMan.  In particular, numpy is used for handling
+# numerical arrays and mathematical operations on them, and
+# matplotlib is used for generating plots of results of calculations
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Here we import the relevant modules from BurnMan.  The burnman
+# module imports several of the most important functionalities of
+# the library, including the ability to make composites, and compute
+# thermoelastic properties of them.  The minerals module includes
+# the mineral physical parameters for the predefined minerals in
+# BurnMan
+import burnman
+from burnman import minerals
+
+# This is the first actual work done in this example.  We define
+# composite object and name it "rock".  A composite is made by
+# giving burnman.composite a list of minerals and their molar fractions.
+# Here "rock" has two constituent minerals: it is 80% Mg perovskite
+# and 20% periclase.  More minerals may be added by simply extending
+# the list given to burnman.composite
+rock = burnman.Composite([0.8, 0.2], [minerals.SLB_2011.mg_perovskite(),
+                                          minerals.SLB_2011.periclase()])
+
+
+# Here we create and load the PREM seismic velocity model, which will be
+# used for comparison with the seismic velocities of the "rock" composite
+seismic_model = burnman.seismic.PREM()
+
+# We create an array of 20 depths at which we want to evaluate PREM, and then
+# query the seismic model for the pressure, density, P wave speed, S wave
+# speed, and bulk sound velocity at those depths
+depths = np.linspace(750e3, 2800e3, 20)
+pressure, seis_rho, seis_vp, seis_vs, seis_vphi = seismic_model.evaluate_all_at(depths)
+
+
+# Now we get an array of temperatures at which will be used for computing
+# the seismic properties of the rock.  Here we use the Brown+Shankland (1981)
+# geotherm for mapping pressure to temperature
+temperature = burnman.geotherm.brown_shankland(pressure)
+
+
+# At this point we want to tell the rock which equation of state to use for
+# its thermoelastic calculations. In general, we recommend the 'slb3'
+# equation of state as the most self-consistent model.  The parameters from
+# the SLB_2011 mineral library are fit using this model.
+rock.set_method('slb3')
+
+# Here is the step which does the heavy lifting.  burnman.velocities_from_rock
+# sets the state of the rock at each of the pressures and temperatures defined,
+# then calculates the elastic moduli and density of each individual phase.  After that,
+# it performs elastic averaging on the phases to get a single bulk and shear
+# modulus for the rock.  This averaging scheme defaults to Voigt-Reuss-Hilli,
+# but see example_averaging.py for other options.  Finally, it calculates the seismic
+# wave speeds for the whole rock.  It returns a tuple of density, p-wave velocity
+# s-wave velocity, bulk sound speed, bulk modulus, and shear modulus.
+density, vp, vs, vphi, K, G = burnman.velocities_from_rock(rock, pressure, temperature)
+
+
+# All the work is done except the plotting!  Here we want to plot the seismic wave
+# speeds and the density against PREM using the matplotlib plotting tools.  We make
+# a 2x2 array of plots.  The fourth subplot plots the geotherm used for this calculation.
+
+# First, we plot the s-wave speed verses the PREM s-wave speed
+plt.subplot(2,2,1)
+plt.plot(pressure/1.e9,vs/1.e3,color='b',linestyle='-',marker='o', markerfacecolor='b',markersize=4,label='computation')
+plt.plot(pressure/1.e9,seis_vs/1.e3,color='k',linestyle='-',marker='o', markerfacecolor='k',markersize=4,label='reference')
+plt.title("S wave speed (km/s)")
+plt.xlim(min(pressure)/1.e9,max(pressure)/1.e9)
+plt.legend(loc='lower right')
+plt.ylim(5,8.0)
+
+# Next, we plot the p-wave speed verses the PREM p-wave speed
+plt.subplot(2,2,2)
+plt.plot(pressure/1.e9,vp/1.e3,color='b',linestyle='-',marker='o',markerfacecolor='b',markersize=4)
+plt.plot(pressure/1.e9,seis_vp/1.e3,color='k',linestyle='-',marker='o',markerfacecolor='k',markersize=4)
+plt.title("P wave speed (km/s)")
+plt.xlim(min(pressure)/1.e9,max(pressure)/1.e9)
+plt.ylim(10,16)
+
+# Next, we plot the density verses the PREM density
+plt.subplot(2,2,3)
+plt.plot(pressure/1.e9,density/1.e3,color='b',linestyle='-',marker='o', markerfacecolor='b',markersize=4)
+plt.plot(pressure/1.e9,seis_rho/1.e3,color='k',linestyle='-',marker='o', markerfacecolor='k',markersize=4)
+plt.xlim(min(pressure)/1.e9,max(pressure)/1.e9)
+plt.xlabel("Pressure (GPa)")
+plt.title("density (kg/m^3)")
+
+
+# Finally, we plot the goetherm used
+plt.subplot(2,2,4)
+plt.plot(pressure/1e9,temperature,color='r',linestyle='-',marker='o',markerfacecolor='r',markersize=4)
+plt.xlim(min(pressure)/1.e9,max(pressure)/1.e9)
+plt.xlabel("Pressure (GPa)")
+plt.title("temperature (K)")
+
+# At long last, we show the results!  We are done!
+plt.show()
---
+++ tutorial/step_3.py
@@ -0,0 +1,144 @@
+# BurnMan - a lower mantle toolkit
+# Copyright (C) 2014, Heister, T., Unterborn, C., Rose, I. and Cottaar, S.
+# Released under GPL v2 or later.
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.ma as ma
+from numpy.random import normal
+from scipy import interpolate
+from matplotlib.colors import LinearSegmentedColormap
+
+import burnman
+from burnman import minerals
+from burnman.mineral_helpers import HelperSolidSolution
+
+
+def realize_mineral( mineral ):
+    K_prime_std_dev = 0.3
+    G_prime_std_dev = 0.3
+    mineral.params['Kprime_0'] = mineral.params['Kprime_0'] + normal(scale=K_prime_std_dev)
+    mineral.params['Gprime_0'] = mineral.params['Gprime_0'] + normal(scale=G_prime_std_dev)
+    return mineral
+
+def realize_rock():
+
+    #approximate four component pyrolite model
+    x_pv = 0.67
+    x_fp = 0.33
+    pv_fe_num = 0.07
+    fp_fe_num = 0.2
+
+    mg_perovskite = minerals.SLB_2011_ZSB_2013.mg_perovskite(); realize_mineral(mg_perovskite)
+    fe_perovskite = minerals.SLB_2011_ZSB_2013.fe_perovskite(); realize_mineral(fe_perovskite)
+    wuestite = minerals.SLB_2011_ZSB_2013.wuestite(); realize_mineral(wuestite)
+    periclase = minerals.SLB_2011_ZSB_2013.periclase(); realize_mineral(periclase)
+
+    perovskite = HelperSolidSolution( [ mg_perovskite, fe_perovskite], [1.0-pv_fe_num, pv_fe_num])
+    ferropericlase = HelperSolidSolution( [ periclase, wuestite], [1.0-fp_fe_num, fp_fe_num])
+
+    mantle_rock = burnman.Composite( [x_pv, x_fp], [perovskite, ferropericlase] )
+    mantle_rock.set_method('slb3')
+
+    return mantle_rock
+
+#set up the seismic model
+seismic_model = burnman.seismic.PREM()
+npts = 10
+depths = np.linspace(850e3,2700e3, npts)
+pressure, seis_rho, seis_vp, seis_vs, seis_vphi = seismic_model.evaluate_all_at(depths)
+n_realizations = 100
+pressures_sampled = np.linspace(pressure[0], pressure[-1], 20*len(pressure))
+
+outfile = open('uncertainty.dat', 'w')
+for i in range(n_realizations):
+
+    print "realization", i+1
+    try:
+      #create the ith model
+      rock = realize_rock()
+      temperature = burnman.geotherm.brown_shankland(pressure)
+
+      #calculate the seismic observables
+      rho, vp, vs, vphi, K, G = \
+        burnman.velocities_from_rock(rock, pressure, temperature, burnman.averaging_schemes.HashinShtrikmanAverage())
+
+      #interpolate to a higher resolution line
+      func_rho = interpolate.interp1d(pressure, rho)
+      func_vs = interpolate.interp1d(pressure, vs)
+      func_vphi = interpolate.interp1d(pressure, vphi)
+
+      pressure_list = pressures_sampled
+      density_list = func_rho(pressures_sampled)
+      vs_list = func_vs(pressures_sampled)
+      vphi_list = func_vphi(pressures_sampled)
+
+
+      data = zip(pressure_list, vs_list, vphi_list, density_list)
+      np.savetxt(outfile,data,fmt='%.10e',delimiter='\t')
+
+    except ValueError:
+      print "failed, skipping"
+
+outfile.close()
+data = np.loadtxt('uncertainty.dat')
+pressure_list = data[:,0]
+density_list = data[:,3]
+vs_list = data[:,1]
+vphi_list = data[:,2]
+
+density_hist,rho_xedge,rho_yedge = np.histogram2d(pressure_list, density_list, bins=len(pressures_sampled), normed = True)
+vs_hist,vs_xedge,vs_yedge = np.histogram2d(pressure_list, vs_list, bins=len(pressures_sampled), normed = True)
+vphi_hist,vphi_xedge,vphi_yedge = np.histogram2d(pressure_list, vphi_list, bins=len(pressures_sampled), normed = True)
+
+
+vs_xedge = vs_xedge/1.e9
+vphi_xedge = vphi_xedge/1.e9
+rho_xedge = rho_xedge/1.e9
+vs_yedge = vs_yedge/1.e3
+vphi_yedge = vphi_yedge/1.e3
+rho_yedge = rho_yedge/1.e3
+
+left_edge = min(vs_xedge[0], vphi_xedge[0], rho_xedge[0])
+right_edge = max(vs_xedge[-1], vphi_xedge[-1], rho_xedge[-1])
+bottom_edge= 4.3
+top_edge=11.3
+aspect_ratio = (right_edge-left_edge)/(top_edge-bottom_edge)
+gamma = 0.8  #Mess with this to change intensity of colormaps near the edges
+
+#do some setup for the figure
+plt.subplots_adjust(wspace=0.3)
+
+plt.subplot(111, aspect='equal')
+plt.xlim(left_edge, right_edge)
+plt.ylim(bottom_edge, top_edge)
+plt.xlabel('Pressure (GPa)')
+plt.ylabel('Wave Speed (km/s)')
+
+
+#plot v_s
+vs_hist = ma.masked_where(vs_hist <= 0.0, vs_hist)
+c = LinearSegmentedColormap.from_list('vphi', [ (0, '#ffffff'), (0.2, '#eff3ff'), (0.4, '#bdd7e7'), (0.6, '#6baed6'), (0.8, '#3182bd'), (1.0, '#08519c') ] , gamma=gamma)
+c.set_bad('w', alpha=1.0)
+plt.imshow(vs_hist.transpose(), origin='low', cmap=c,  interpolation='gaussian', alpha=.7,\
+       aspect=aspect_ratio, extent=[vs_xedge[0], vs_xedge[-1], vs_yedge[0], vs_yedge[-1]])
+plt.plot(pressure/1.e9,seis_vs/1.e3,linestyle="--",color='k',linewidth=2.0,label='PREM')
+
+#plot v_phi
+vphi_hist = ma.masked_where(vphi_hist <= 0.0, vphi_hist)
+c = LinearSegmentedColormap.from_list('vphi', [ (0, '#ffffff'), (0.2, '#fee5d9'), (0.4, '#fcae91'), (0.6, '#fb6a4a'), (0.8, '#de2d26'), (1.0, '#a50f15') ] , gamma=gamma)
+c.set_bad('w', alpha=1.0)
+plt.imshow(vphi_hist.transpose(), origin='low', cmap=c, interpolation='gaussian', alpha=.7, \
+       aspect=aspect_ratio, extent=[vphi_xedge[0], vphi_xedge[-1], vphi_yedge[0], vphi_yedge[-1]])
+plt.plot(pressure/1.e9,seis_vphi/1.e3,linestyle="--",color='k',linewidth=2.0,label='PREM')
+
+#plot density
+density_hist = ma.masked_where(density_hist <= 0.0, density_hist)
+c = LinearSegmentedColormap.from_list('vphi', [ (0, '#ffffff'), (0.2, '#edf8e9'), (0.4, '#bae4b3'), (0.6, '#74c476'), (0.8, '#31a354'), (1.0, '#006d2c') ] , gamma=0.2)
+c.set_bad('w', alpha=1.0)
+plt.imshow(density_hist.transpose(), origin='low', cmap=c, interpolation = 'gaussian', alpha=.7,\
+       aspect=aspect_ratio, extent=[rho_xedge[0], rho_xedge[-1], rho_yedge[0], rho_yedge[-1]])
+plt.plot(pressure/1.e9,seis_rho/1.e3,linestyle="--",color='k',linewidth=2.0,label='PREM')
+
+plt.show()
