dc2d2fcf17cd91f5d9f9267a480e5a68b81e09c8
ian-r-rose <ian.r.rose@gmail.com>
2014-06-17 19:57:28 -0700

    Text for step_3

--- tutorial/step_3.py
+++ tutorial/step_3.py
@@ -2,6 +2,31 @@
 # Copyright (C) 2014, Heister, T., Unterborn, C., Rose, I. and Cottaar, S.
 # Released under GPL v2 or later.
 
+"""
+
+CIDER 2014 BurnMan Tutorial --- step 3
+
+In the previous two steps of the tutorial we tried to find a very simple 
+mineralogical model that best fit the 1D seismic model PREM.  But we 
+know that there is consideral uncertainty in many of the mineral
+physical parameters that control how the elastic properties of minerals 
+change with pressure and temperature.  In this step we explore how uncertainties
+in these parameters might affect the conclusions you draw.
+
+The strategy here is to make many different ``realizations'' of the rock 
+that you determined was the closest fit to PREM, where each realization 
+has its mineral physical parameters perturbed by a small amount, hopefully 
+related to the uncertainty in that parameter.  In particular, we will
+look at how perturbations to K' and G' (the pressure derivatives of the 
+bulk and shear modulus, respectively) change the calculated 1D seismic
+profiles.
+
+
+This script may be run by typing
+    
+    python step_3.py
+
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,39 +37,49 @@ from matplotlib.colors import LinearSegmentedColormap
 
 import burnman
 from burnman import minerals
-from burnman.mineral_helpers import HelperSolidSolution
 
+
+"""
+Here we define a function realize_mineral() which takes a mineral
+as a paramter, perturbs its K' and G' parameters, then returns
+the modified mineral.  We perturb it by drawing from a normal 
+ditribution with a standard deviation given by K_prime_std_dev
+and G_prime_std_dev.  These parameters are currently set to very small
+values but you will want to put in realistic values (perhaps 0.1-0.2)
+"""
 
 def realize_mineral( mineral ):
-    K_prime_std_dev = 0.2  #<----------------- One sigma uncertainty in K prime
-    G_prime_std_dev = 0.2  #<----------------- One sigma uncertainty in G prime
+    K_prime_std_dev = 0.3  #<----------------- One sigma uncertainty in K prime
+    G_prime_std_dev = 0.3  #<----------------- One sigma uncertainty in G prime
 
     mineral.params['Kprime_0'] = mineral.params['Kprime_0'] + normal(scale=K_prime_std_dev)
     mineral.params['Gprime_0'] = mineral.params['Gprime_0'] + normal(scale=G_prime_std_dev)
     return mineral
 
+"""
+Here we define a function realize_rock(), which creates the realization of
+our mantle model.  First it makes a mineral, then calls realize_mineral()
+on that mineral to perturb its values, and then makes the rock out of
+those perturbed minerals.  Put in your preferred mantle model from steps 
+1 and 2 here.
+"""
+
 def realize_rock():
 
-    #approximate four component pyrolite model
-    x_pv = 0.7    #<---------------------------- Fraction of perovskite in preferred model
-    x_fp = 1.0-x_pv   
-    pv_fe_num = 0.05   #<------------------------- Fraction of iron in perovskite
-    fp_fe_num = 0.3    #<------------------------- Franction of iron in ferropericlase
+    phase_1_fraction = 0.5
+    phase_2_fraction = 1.0-phase_1_fraction
 
-    mg_perovskite = minerals.SLB_2011.mg_perovskite(); realize_mineral(mg_perovskite)
-    fe_perovskite = minerals.SLB_2011.fe_perovskite(); realize_mineral(fe_perovskite)
-    wuestite = minerals.SLB_2011.wuestite(); realize_mineral(wuestite)
-    periclase = minerals.SLB_2011.periclase(); realize_mineral(periclase)
+    phase_1 = minerals.SLB_2011.stishovite(); realize_mineral(phase_1)
+    phase_2 = minerals.SLB_2011.wuestite(); realize_mineral(phase_2)
 
-    perovskite = HelperSolidSolution( [ mg_perovskite, fe_perovskite], [1.0-pv_fe_num, pv_fe_num])
-    ferropericlase = HelperSolidSolution( [ periclase, wuestite], [1.0-fp_fe_num, fp_fe_num])
-
-    mantle_rock = burnman.Composite( [x_pv, x_fp], [perovskite, ferropericlase] )
+    mantle_rock = burnman.Composite( [phase_1_fraction, phase_2_fraction], [phase_1, phase_2] )
     mantle_rock.set_method('slb3')
 
     return mantle_rock
 
-#set up the seismic model
+# Again, we set up the seismic model and ask for its properties
+# in the lower mantle.  Basically the same thing as in steps 1 and 2.
+
 seismic_model = burnman.seismic.PREM()
 min_depth = 850.e3
 max_depth = 2800.e3
@@ -53,24 +88,30 @@ depths = np.linspace(min_depth, max_depth, n_depths)
 pressure, seis_rho, seis_vp, seis_vs, seis_vphi = seismic_model.evaluate_all_at(depths)
 pressures_sampled = np.linspace(pressure[0], pressure[-1], 20*len(pressure))
 
+temperature = burnman.geotherm.brown_shankland(pressure)
 
 
-n_realizations = 30
+"""
+Finally, we make 500 realizations of the rock and save the seismic profiles
+for later visualization.  
+"""
+
+n_realizations = 300
 outfile = open('uncertainty.dat', 'w')
 
 for i in range(n_realizations):
 
     print "realization", i+1
     try:
-      #create the ith model
+      # We call the realize_rock() to create the ith model
       rock = realize_rock()
-      temperature = burnman.geotherm.brown_shankland(pressure)
 
-      #calculate the seismic observables
+      # Calculate the wavespeed profiles, just as before.
       rho, vp, vs, vphi, K, G = \
         burnman.velocities_from_rock(rock, pressure, temperature, burnman.averaging_schemes.VoigtReussHill())
 
-      #interpolate to a higher resolution line
+      # This interpolates the resulting densities and wavespeeds\
+      # to a higher resolution line to make the plot look nicer.
       func_rho = interpolate.interp1d(pressure, rho)
       func_vs = interpolate.interp1d(pressure, vs)
       func_vphi = interpolate.interp1d(pressure, vphi)
@@ -80,20 +121,36 @@ for i in range(n_realizations):
       vs_list = func_vs(pressures_sampled)
       vphi_list = func_vphi(pressures_sampled)
 
-
+      # Save the output to a file
       data = zip(pressure_list, vs_list, vphi_list, density_list)
       np.savetxt(outfile,data,fmt='%.10e',delimiter='\t')
 
+    # It is possible for the Birch-Murnaghan equation of state to go unstable for
+    # some values of the parameters, which can make it fail.  If that happens, we
+    # simply disregard this realization of the rock.
     except ValueError:
       print "failed, skipping"
 
+
+"""
+The rest of this script is concerned with plotting the results so that 
+you may visualize the uncertainty space that we have sampled with our
+mineral model.  In different colors we see 
+
+In general, this plotting is more complex than the ones in step 1 and step 2
+and you may safely ignore it.  
+"""
+
+# Read in the data from uncertainty.dat
 outfile.close()
 data = np.loadtxt('uncertainty.dat')
 pressure_list = data[:,0]
-density_list = data[:,3]
 vs_list = data[:,1]
 vphi_list = data[:,2]
+density_list = data[:,3]
 
+
+# Create 2D histograms showing the spread of the data
 density_hist,rho_xedge,rho_yedge = np.histogram2d(pressure_list, density_list, bins=len(pressures_sampled), normed = True)
 vs_hist,vs_xedge,vs_yedge = np.histogram2d(pressure_list, vs_list, bins=len(pressures_sampled), normed = True)
 vphi_hist,vphi_xedge,vphi_yedge = np.histogram2d(pressure_list, vphi_list, bins=len(pressures_sampled), normed = True)
@@ -108,13 +165,10 @@ rho_yedge = rho_yedge/1.e3
 
 left_edge = min(vs_xedge[0], vphi_xedge[0], rho_xedge[0])
 right_edge = max(vs_xedge[-1], vphi_xedge[-1], rho_xedge[-1])
-bottom_edge= 4.3
+bottom_edge= 4.0
 top_edge=11.3
 aspect_ratio = (right_edge-left_edge)/(top_edge-bottom_edge)
-gamma = 0.8  #Mess with this to change intensity of colormaps near the edges
-
-#do some setup for the figure
-plt.subplots_adjust(wspace=0.3)
+gamma = 0.5  #Mess with this to change intensity of colormaps near the edges
 
 plt.subplot(111, aspect='equal')
 plt.xlim(left_edge, right_edge)
@@ -125,19 +179,19 @@ plt.ylabel('Wave Speed (km/s)')
 
 #plot v_s
 vs_hist = ma.masked_where(vs_hist <= 0.0, vs_hist)
-c = LinearSegmentedColormap.from_list('vphi', [ (0, '#ffffff'), (0.2, '#eff3ff'), (0.4, '#bdd7e7'), (0.6, '#6baed6'), (0.8, '#3182bd'), (1.0, '#08519c') ] , gamma=gamma)
+c = LinearSegmentedColormap.from_list('vphi', [ (0, '#ffffff'), (0.2, '#eff3ff'), (0.4, '#bdd7e7'), (0.6, '#6baed6'), (0.8, '#3182bd'), (1.0, '#08519c') ] , gamma=0.5)
 c.set_bad('w', alpha=1.0)
 plt.imshow(vs_hist.transpose(), origin='low', cmap=c,  interpolation='gaussian', alpha=.7,\
        aspect=aspect_ratio, extent=[vs_xedge[0], vs_xedge[-1], vs_yedge[0], vs_yedge[-1]])
-plt.plot(pressure/1.e9,seis_vs/1.e3,linestyle="--",color='k',linewidth=2.0,label='PREM')
+plt.plot(pressure/1.e9,seis_vs/1.e3,linestyle="--",color='b',linewidth=2.0,label='Vs')
 
 #plot v_phi
 vphi_hist = ma.masked_where(vphi_hist <= 0.0, vphi_hist)
-c = LinearSegmentedColormap.from_list('vphi', [ (0, '#ffffff'), (0.2, '#fee5d9'), (0.4, '#fcae91'), (0.6, '#fb6a4a'), (0.8, '#de2d26'), (1.0, '#a50f15') ] , gamma=gamma)
+c = LinearSegmentedColormap.from_list('vphi', [ (0, '#ffffff'), (0.2, '#fee5d9'), (0.4, '#fcae91'), (0.6, '#fb6a4a'), (0.8, '#de2d26'), (1.0, '#a50f15') ] , gamma=0.5)
 c.set_bad('w', alpha=1.0)
 plt.imshow(vphi_hist.transpose(), origin='low', cmap=c, interpolation='gaussian', alpha=.7, \
        aspect=aspect_ratio, extent=[vphi_xedge[0], vphi_xedge[-1], vphi_yedge[0], vphi_yedge[-1]])
-plt.plot(pressure/1.e9,seis_vphi/1.e3,linestyle="--",color='k',linewidth=2.0,label='PREM')
+plt.plot(pressure/1.e9,seis_vphi/1.e3,linestyle="--",color='r',linewidth=2.0,label='Vphi')
 
 #plot density
 density_hist = ma.masked_where(density_hist <= 0.0, density_hist)
@@ -145,6 +199,8 @@ c = LinearSegmentedColormap.from_list('vphi', [ (0, '#ffffff'), (0.2, '#edf8e9')
 c.set_bad('w', alpha=1.0)
 plt.imshow(density_hist.transpose(), origin='low', cmap=c, interpolation = 'gaussian', alpha=.7,\
        aspect=aspect_ratio, extent=[rho_xedge[0], rho_xedge[-1], rho_yedge[0], rho_yedge[-1]])
-plt.plot(pressure/1.e9,seis_rho/1.e3,linestyle="--",color='k',linewidth=2.0,label='PREM')
+plt.plot(pressure/1.e9,seis_rho/1.e3,linestyle="--",color='g',linewidth=2.0,label='Density')
+
+plt.legend(loc = 'lower right')
 
 plt.show()
