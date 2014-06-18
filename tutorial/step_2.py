f611903f0e5855ff2c78a3ba7f0fa12d9969864b
ian-r-rose <ian.r.rose@gmail.com>
2014-06-17 19:11:40 -0700

    Add lots of text, modifications for step_2

--- tutorial/step_2.py
+++ tutorial/step_2.py
@@ -3,8 +3,26 @@
 # Released under GPL v2 or later.
 
 """
+
+CIDER 2014 BurnMan Tutorial --- step 2
+
+In this second part of the tutorial we try to get a closer fit to our
+1D seismic reference model.  In the simple Mg, Si, and O model that 
+we used in step 1 there was one free parameter, namely phase_1_fraction,
+which goes between zero and one.  
+
+In this script we want to explore how good of a fit to PREM we can get 
+by varying this fraction. We create a simple function that calculates 
+a misfit between PREM and our mineral model as a function of phase_1_fraction,
+and then plot this misfit function to try to find a ``best model''.
+
+This script may be run by typing
+    
+    python step_2.py
+
 """
 
+# The imports here are identical to those from step 1
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,6 +30,9 @@ import burnman
 from burnman import minerals
 
 
+# Again, we load the PREM seismic model and query it for pressure,
+# density, and elastic properties at lower mantle depths. This, too,
+# is identical to step 1
 n_depths = 20
 min_depth = 850.e3
 max_depth = 2800.e3
@@ -21,29 +42,68 @@ seismic_model = burnman.seismic.PREM()
 pressure, seis_rho, seis_vp, seis_vs, seis_vphi = seismic_model.evaluate_all_at(depths)
 temperature = burnman.geotherm.brown_shankland(pressure)
 
-def material_error(amount_perovskite):
-    rock =  ?????          #<-------------------- Create a rock
-    rock.set_method('slb3')
-    density, vp, vs, vphi, K, G =   ???? #<---------------- Calculate the elastic properties of the rock
 
+"""
+This is the main workhorse of this script.  We define the function ``misfit''
+which takes a single parameter phase_1_fraction.  We create the rock we want,
+calculate its elastic properties, and then calculate an L2 misfit between 
+the calculated profiles for Vs, Vphi, and density and those for PREM.
+
+Here again is our model with stishovite and wuestite.  Instead of that, you
+will want to copy the ``rock'' you created in step 1.  If you experimented with
+other rocks than those with Mg perovskite and periclase, you can also try those.
+"""
+
+def misfit(phase_1_fraction):
+
+    # Here we define the rock as before. 
+    phase_2_fraction = 1.0-phase_1_fraction
+    rock = burnman.Composite([phase_1_fraction, phase_2_fraction], 
+                             [minerals.SLB_2011.stishovite(), minerals.SLB_2011.wuestite()])
+
+    # Just as in step 1, we want to set which equation of state we use,
+    # then call burnman.velocities_from_rock, which evaluates the 
+    # elastic properties and seismic velocities at the predefined 
+    # pressures and temperatures
+    rock.set_method('slb3')
+    density, vp, vs, vphi, K, G = burnman.velocities_from_rock(rock, pressure, temperature)
+
+    # Since we will call this misfit function many times, we may be interested
+    # in a status report.  These lines print some debug output so we 
+    # can keep track of what the script is doing.
     print "Calculations are done for:"
     rock.debug_print()
 
+    # Here we integrate an L2 difference with depth between our calculated seismic
+    # profiles and PREM.  We then return those misfits.
     [vs_err, vphi_err, rho_err]=burnman.compare_l2(depths,[vs,vphi,density],[seis_vs,seis_vphi,seis_rho])
 
     return vs_err, vphi_err, rho_err
 
-xx=np.linspace(0.0, 1.0, 50)
-errs=np.array([material_error(x) for x in xx])
-yy_vs=errs[:,0]
-yy_vphi=errs[:,1]
-yy_rho=errs[:,2]
+"""
 
-plt.plot (xx,yy_vs,"r-x",label=("vs error"))
-plt.plot (xx,yy_vphi,"b-x",label=("vphi error"))
-plt.plot (xx,yy_rho,"g-x",label=("rho error"))
+With the misfit function now defined, we can call it many times for different
+phase_1_fraction values, and see how good of a fit we can get.
+
+"""
+
+# We create the array ``fraction'', which has 101 fractions between
+# zero and one, and call our misfit function for each of those fractions.
+fraction =np.linspace(0.0, 1.0, 101)
+errs=np.array([misfit(f) for f in fraction])
+vs_misfit=errs[:,0]
+vphi_misfit=errs[:,1]
+rho_misfit=errs[:,2]
+
+# Finally, we plot the misfits against the phase_1_fraction. You will probably
+# find that it is difficult to fit shear wave speed, bulk sound speed, and
+# density all at the same time.
+
+plt.plot (fraction,vs_misfit,"r-x",label=("Vs misfit"))
+plt.plot (fraction,vphi_misfit,"b-x",label=("Vphi misfit"))
+plt.plot (fraction,rho_misfit,"g-x",label=("Density misfit"))
 plt.yscale('log')
-plt.xlabel('% Perovskite')
-plt.ylabel('Error')
+plt.xlabel('% Phase 1')
+plt.ylabel('Misfit')
 plt.legend()
 plt.show()
