a83d427bb6a1415886548d5fc63eb4d2c52b5bfd
Timo Heister <timo.heister@gmail.com>
2014-07-11 09:13:02 -0700

    thermal exp, c_v, c_p, Model improvements
    remove all references to main while we are at it

--- burnman/model.py
+++ burnman/model.py
@@ -19,6 +19,9 @@ class Model:
 
     """
     def __init__(self, rock, p, T, avgscheme):
+        assert(len(p) == len(T))
+        assert(len(p) > 0)
+
         self.rock = rock
         self.p = p
         self.T = T
@@ -35,6 +38,9 @@ class Model:
         self.mat_vp = None
         self.mat_vs = None
         self.mat_vphi = None
+        self.alpha = None
+        self.c_p = None
+        self.c_v = None
 
     def v_s(self):
         self.compute_velocities_()
@@ -66,24 +72,98 @@ class Model:
             self.mat_G = np.array([m.G for m in self.avgmoduli])
         return self.mat_G
 
+    def thermal_expansivity(self):
+        if self.alpha is None:
+            self.calc_moduli_()
+            n_pressures = len(self.p)
+            self.alpha = np.zeros(n_pressures)
+            for idx in range(n_pressures):
+                V = np.array([m['V'] for m in self.moduli[idx]])
+                alpha = np.array([m['alpha'] for m in self.moduli[idx]])
+                self.alpha[idx] = self.avgscheme.average_thermal_expansivity(V, alpha)
+
+        return self.alpha
+
+    def heat_capacity_p(self):
+        self.calc_heat_capacities_()
+        return self.c_p
+
+    def heat_capacity_v(self):
+        self.calc_heat_capacities_()
+        return self.c_v
 
     def calc_moduli_(self):
+        """
+        Internal function to compute the moduli if necessary.
+        """
         if self.moduli is None:
-            self.moduli = main.calculate_moduli(self.rock, self.p, self.T)
+                self.moduli = [[] for p in self.p]
+
+                for idx in range(len(self.p)):
+                    self.rock.set_state(self.p[idx], self.T[idx])
+                    (fractions, minerals) = self.rock.unroll()
+                    for (fraction, mineral) in zip(fractions, minerals):
+                        e = {}
+                        e['fraction'] = fraction
+                        e['V'] = fraction * mineral.molar_volume()
+                        e['K'] = mineral.adiabatic_bulk_modulus()
+                        e['G'] = mineral.shear_modulus()
+                        e['rho'] = mineral.molar_mass() / mineral.molar_volume()
+                        e['alpha'] = mineral.thermal_expansivity()
+                        e['c_v'] = mineral.heat_capacity_v()
+                        self.moduli[idx].append(e)
 
     def avg_moduli_(self):
+        """
+        Internal function to average moduli if necessary.
+        """
         if self.avgmoduli is None:
             self.calc_moduli_()
-            self.avgmoduli = main.average_moduli(self.moduli, self.avgscheme)
+            n_pressures = len(self.p)
+            self.avgmoduli = [{} for i in range(n_pressures)]
+
+            for idx in range(n_pressures):
+                mods  = self.moduli[idx]
+
+                fractions = np.array([e['fraction'] for e in mods])
+                V_frac = np.array([m['V'] for m in mods])
+                K_ph = np.array([m['K'] for m in mods])
+                G_ph = np.array([m['G'] for m in mods])
+                rho_ph = np.array([m['rho'] for m in mods])
+
+                self.avgmoduli[idx]['V'] = sum(V_frac)
+                self.avgmoduli[idx]['K'] = self.avgscheme.average_bulk_moduli(V_frac, K_ph, G_ph)
+                self.avgmoduli[idx]['G'] = self.avgscheme.average_shear_moduli(V_frac, K_ph, G_ph)
+                self.avgmoduli[idx]['rho'] = self.avgscheme.average_density(V_frac, rho_ph)
+
+    def calc_heat_capacities_(self):
+        """
+        Internal function to compute the heat capacities if necessary.
+        """
+        if self.c_p is None:
+            self.calc_moduli_()
+            self.c_v = np.zeros(len(self.p))
+            self.c_p = np.zeros(len(self.p))
+            for idx in range(len(self.p)):
+                fractions = np.array([m['fractions'] for m in self.moduli[idx]])
+                alphas = np.array([m['alpha'] for m in self.moduli[idx]])
+                c_v = np.array([m['c_v'] for m in self.moduli[idx]])
+                c_p = np.array([m['c_p'] for m in self.moduli[idx]])
+                self.c_v[idx] = self.avgscheme.average_heat_capacity_v(self, fractions, c_v)
+                self.c_p[idx] = self.avgscheme.average_heat_capacity_p(self, fractions, c_p)
 
     def compute_velocities_(self):
+        """
+        Internal function to compute the velocities if necessary.
+        """
         if self.mat_vp is None:
             self.avg_moduli_()
-            self.mat_vp, self.mat_vs, self.mat_vphi = main.compute_velocities(self.avgmoduli)
+            self.mat_vs = np.ndarray(len(self.p))
+            self.mat_vp = np.ndarray(len(self.p))
+            self.mat_vphi = np.ndarray(len(self.p))
 
-
-
-
-
-
-
+            for i in range(len(self.p)):
+                mod = self.avgmoduli[i]
+                self.mat_vs[i] = np.sqrt( mod['G'] / mod['rho'])
+                self.mat_vp[i] = np.sqrt( (mod['K'] + 4./3.*mod['G']) / mod['rho'])
+                self.mat_vphi[i] = np.sqrt( mod['K'] / mod['rho'])
