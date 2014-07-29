5b038869cdc2e926c84a047f1f7e27663d7ff099
Bob Myhill <myhill.bob@gmail.com>
2014-07-29 00:24:54 -0700

    Adding non-functional solidsolution.py

---
+++ burnman/modified_tait.py
@@ -0,0 +1,178 @@
+# BurnMan - a lower mantle toolkit
+# Copyright (C) 2012, 2013, Myhill, R., Heister, T., Unterborn, C., Rose, I. and Cottaar, S.
+# Released under GPL v2 or later.
+
+import numpy as np
+import scipy.optimize as opt
+
+import burnman.equation_of_state as eos
+import burnman.birch_murnaghan as bm
+import burnman.debye as debye
+
+
+class MGDBase(eos.EquationOfState):
+    """
+    Base class for a generic modified Tait equation of state.  
+    References for this can be found in Huang and Chow (1974) 
+    and Holland and Powell (2011; followed here).
+    """
+
+    #def __init__(self):
+    #    pass
+
+    def grueneisen_parameter(self, pressure, temperature, volume, params):
+        """
+        Returns grueneisen parameter [unitless] as a function of pressure,
+        temperature, and volume (EQ B6)
+        """
+        return self.__grueneisen_parameter(params['V_0']/volume, params)
+
+    def volume(self, pressure,temperature,params):
+        """
+        Returns volume [m^3] as a function of pressure [Pa] and temperature [K]
+        EQ B7
+        """
+        func = lambda x: bm.birch_murnaghan(params['V_0']/x, params) + \
+            self.__thermal_pressure(temperature, x, params) - \
+            self.__thermal_pressure(300., x, params) - pressure
+        V = opt.brentq(func, 0.5*params['V_0'], 1.5*params['V_0'])
+        return V
+
+    def isothermal_bulk_modulus(self, pressure,temperature,volume, params):
+        """
+        Returns isothermal bulk modulus [Pa] as a function of pressure [Pa],
+        temperature [K], and volume [m^3].  EQ B8
+        """
+        K_T = bm.bulk_modulus(volume, params) + \
+            self.__thermal_bulk_modulus(temperature,volume, params) - \
+            self.__thermal_bulk_modulus(300.,volume, params)  #EQB13
+        return K_T
+
+    #calculate the mgd shear modulus as a function of P, V, and T
+    def shear_modulus(self, pressure, temperature, volume, params):
+        """
+        Returns shear modulus [Pa] as a function of pressure [Pa],
+        temperature [K], and volume [m^3].  EQ B11
+        """
+        if self.order==2:
+            return bm.shear_modulus_second_order(volume,params) + \
+                self.__thermal_shear_modulus(temperature,volume, params) - \
+                self.__thermal_shear_modulus(300.,volume, params) # EQ B11
+        elif self.order==3:
+            return bm.shear_modulus_third_order(volume,params) + \
+                self.__thermal_shear_modulus(temperature,volume, params) - \
+                self.__thermal_shear_modulus(300.,volume, params) # EQ B11
+        else:
+            raise NotImplementedError("")
+
+    #heat capacity at constant volume
+    def heat_capacity_v(self, pressure, temperature, volume, params):
+        """
+        Returns heat capacity at constant volume at the pressure, temperature, and volume [J/K/mol]
+        """
+        Debye_T = self.__debye_temperature(params['V_0']/volume, params)
+        C_v = debye.heat_capacity_v(temperature, Debye_T, params['n'])
+        return C_v
+
+    def thermal_expansivity(self, pressure, temperature, volume , params):
+        """
+        Returns thermal expansivity at the pressure, temperature, and volume [1/K]
+        """
+        C_v = self.heat_capacity_v(pressure,temperature,volume,params)
+        gr = self.__grueneisen_parameter(params['V_0']/volume, params)
+        K = self.isothermal_bulk_modulus(pressure, temperature, volume ,params)
+        alpha = gr * C_v / K / volume
+        return alpha
+
+    #heat capacity at constant pressure
+    def heat_capacity_p(self,pressure, temperature,volume,params):
+        """
+        Returns heat capacity at constant pressure at the pressure, temperature, and volume [J/K/mol]
+        """
+        alpha = self.thermal_expansivity(pressure,temperature,volume,params)
+        gr = self.__grueneisen_parameter(params['V_0']/volume, params)
+        C_v = self.heat_capacity_v(pressure,temperature,volume,params)
+        C_p = C_v*(1. + gr * alpha * temperature)
+        return C_p
+
+    def adiabatic_bulk_modulus(self,pressure,temperature,volume,params):
+        """
+        Returns adiabatic bulk modulus [Pa] as a function of pressure [Pa],
+        temperature [K], and volume [m^3].  EQ D6
+        """
+        K_T= self.isothermal_bulk_modulus(pressure,temperature,volume,params)
+        alpha = self.thermal_expansivity(pressure,temperature,volume,params)
+        gr = self.__grueneisen_parameter(params['V_0']/volume, params)
+        K_S = K_T*(1. + gr * alpha * temperature)
+        return K_S
+
+    def pressure(self, temperature, volume, params):
+        """
+        Returns pressure [Pa] as a function of temperature [K] and volume[m^3]
+        EQ B7
+        """
+        return bm.birch_murnaghan(params['V_0']/volume, params) + \
+                self.__thermal_pressure(temperature,volume, params) - \
+                self.__thermal_pressure(300.,volume, params)
+
+    #calculate the thermal correction to the shear modulus as a function of V, T
+    def __thermal_shear_modulus(self, T, V, params):
+        gr = self.__grueneisen_parameter(params['V_0']/V, params)
+        Debye_T = self.__debye_temperature(params['V_0']/V, params)
+        G_th= 3./5. * ( self.__thermal_bulk_modulus(T,V,params) - \
+                 6*debye.R*T*params['n']/V * gr * debye.debye_fn(Debye_T/T) ) # EQ B10
+        return G_th
+
+    #compute the Debye temperature in K.  Takes the
+    #parameter x, which is V_0/V (molar volumes).
+    #Depends on the reference grueneisen parameter,
+    #the reference Debye temperature, and the factor
+    #q_0, see Matas eq B6
+    def __debye_temperature(self, x, params):
+        return params['Debye_0']*np.exp((params['grueneisen_0']- \
+            self.__grueneisen_parameter(x, params))/params['q_0'])
+
+    #compute the grueneisen parameter with depth, according
+    #to q_0.  Takes x=V_0/V. See Matas eq B6
+    def __grueneisen_parameter(self, x, params):
+        return params['grueneisen_0']*pow(1./x, params['q_0'])
+
+    #calculate isotropic thermal pressure, see
+    # Matas et. al. (2007) eq B4
+    def __thermal_pressure(self,T,V, params):
+        Debye_T = self.__debye_temperature(params['V_0']/V, params)
+        gr = self.__grueneisen_parameter(params['V_0']/V, params)
+        P_th = gr * debye.thermal_energy(T,Debye_T, params['n'])/V
+        return P_th
+
+
+    #calculate the thermal correction for the mgd
+    #bulk modulus (see matas et al, 2007)
+    def __thermal_bulk_modulus(self, T,V,params):
+        gr = self.__grueneisen_parameter(params['V_0']/V, params)
+        Debye_T = self.__debye_temperature(params['V_0']/V, params)
+        K_th = 3.*params['n']*debye.R*T/V * gr * \
+            ((1. - params['q_0'] - 3.*gr)*debye.debye_fn(Debye_T/T)+3.*gr*(Debye_T/T)/(np.exp(Debye_T/T) - 1.)) # EQ B5
+        return K_th
+
+
+class MGD3(MGDBase):
+    """
+    MGD equation of state with third order finite strain expansion for the
+    shear modulus (this should be preferred, as it is more thermodynamically
+    consistent.
+    """
+    def __init__(self):
+        self.order=3
+
+
+class MGD2(MGDBase):
+    """
+    MGD equation of state with second order finite strain expansion for the
+    shear modulus.  In general, this should not be used, but sometimes
+    shear modulus data is fit to a second order equation of state.  In that
+    case, you should use this.  The moral is, be careful!
+    """
+    def __init__(self):
+        self.order=2
+
---
+++ burnman/solidsolution.py
@@ -0,0 +1,237 @@
+# BurnMan - a lower mantle toolkit
+# Copyright (C) 2012, 2013, Heister, T., Unterborn, C., Rose, I. and Cottaar, S.
+# Released under GPL v2 or later.
+
+import warnings
+
+import numpy as np
+
+# Kronecker delta
+def kd(x, y):
+    if x == y:
+        return 1
+    else:
+        return 0
+
+import burnman.symmetric_model as sym
+import burnman.asymmetric_model as asym
+
+class SolidSolution(Mineral):
+    """
+    This is the base class for all solid solutions. 
+    States of the solid solution can only be queried after setting 
+    the pressure and temperature using set_state(). 
+    The method for computing properties of
+    the solution is set using set_method(), which should be done
+    once after creating the material.
+
+    This class is available as ``burnman.SolidSolution``.
+
+    If deriving from this class, set the properties in self.params
+    to the desired values. For more complicated materials you
+    can overwrite set_state(), change the params and then call
+    set_state() from this class.
+
+    All the solid solution parameters are expected to be in SI units.  This
+    means that the interaction parameters should be in J/mol, with the T 
+    and P derivatives in J/K/mol and m^3/(mol molecule).
+    """
+
+    def __init__(self, base_material, molar_fraction, site_occupancy, interaction_parameter, van_laar_parameter):
+        self.base_material = base_material
+        self.molar_fraction = molar_fraction
+        assert(len(base_material) == len(molar_fraction))
+        assert(len(base_material) == len(van_laar_parameter))
+        assert(sum(molar_fraction) > 0.9999)
+        assert(sum(molar_fraction) < 1.0001)
+
+        # make sure the number of atoms in each endmember is the same
+        # (when we start adding minerals with vacancies
+        # this will need to be changed).
+        for m in base_material:
+            if(base_material[0].params.has_key('n')):
+                assert(m.params['n'] == base_material[0].params['n'])
+
+    # Set the state of all the endmembers
+    def set_state(self, pressure, temperature):
+        for mat in self.base_materials:
+            mat.method = self.method
+            mat.set_state(pressure, temperature)
+
+        itrange = range(0, len(self.base_material))
+
+        self.params = {}
+
+        # NEEDS CHANGING FROM HERE!!!
+        # Description of RTlngamma terms given by the asymmetric formalism
+        # is included in Holland and Powell (2003)
+
+        # the volume is calculated in the same way as Gex (p.493),
+        # replacing Wij with Wvij
+        Vex=-2.*sum([ sum([ self.molar_fraction[i]*self.van_laar_parameter[i]*self.molar_fraction[j]*self.van_laar_parameter[j]*self.interaction_parameter[i][2] / (self.van_laar_parameter[i] + self.van_laar_parameter[j]) for j in range(i, len(self.base_material)) ]) for i in range(0, len(self.base_material)-1) ])/sum([ self.van_laar_parameter[l]*self.molar_fraction[l] for l in itrange ])
+        V= sum([ self.base_materials[i].molar_volume() * self.molar_fraction[i] for i in itrange ]) + Vex
+
+        # the volume is calculated in the same way as Gex (p.493),
+        # replacing Wij with Wvij
+        # NEED TO CORRECT INDICES FOR INT PARAM
+        # W[i][j] == W[(itrange-1)*i+(j-1)]
+        Vex=-2.*sum([ sum([ self.molar_fraction[i]*self.van_laar_parameter[i]*self.molar_fraction[j]*self.van_laar_parameter[j]*self.interaction_parameter[(itrange-1)*i+(j-1)][2] / (self.van_laar_parameter[i] + self.van_laar_parameter[j]) for j in range(i, len(self.base_material)) ]) for i in range(0, len(self.base_material)-1) ])/sum([ self.van_laar_parameter[l]*self.molar_fraction[l] for l in itrange ])
+        V= sum([ self.base_materials[i].molar_volume() * self.molar_fraction[i] for i in itrange ]) + Vex
+
+
+        # Same for the Gibbs free energy
+        # NEED TO CORRECT INDICES FOR INT PARAM
+        Gex=-2.*sum([ sum([ self.molar_fraction[i]*self.van_laar_parameter[i]*self.molar_fraction[j]*self.van_laar_parameter[j]*(pressure*self.interaction_parameter[(itrange-1)*i+(j-1)][0] + temperature*self.interaction_parameter[(itrange-1)*i+(j-1)][1] + pressure*self.interaction_parameter[(itrange-1)*i+(j-1)][2]) / (self.van_laar_parameter[i] + self.van_laar_parameter[j]) for j in range(i, len(self.base_material)) ]) for i in range(0, len(self.base_material)-1) ])/sum([ self.van_laar_parameter[l]*self.molar_fraction[l] for l in itrange ])
+        G= sum([ self.base_materials[i].molar_gibbs() * self.molar_fraction[i] for i in itrange ]) + Gex
+
+        
+
+        
+
+        for prop in self.base_materials[0].params:
+           try:
+               self.params[prop] = sum([ self.base_materials[i].params[prop] * self.molar_fraction[i] for i in itrange ])
+           except TypeError:
+               #if there is a type error, it is probably a string.  Just go with the value of the first base_material.
+               self.params[prop] = self.base_materials[0].params[prop]
+        Mineral.set_state(self, pressure, temperature)
+
+    # BELOW IS FROM THE MINERAL CLASS.
+    def set_method(self, method):
+        """
+        Set the equation of state to be used for this mineral.
+        Takes a string corresponding to any of the predefined
+        equations of state:  'bm2', 'bm3', 'mgd2', 'mgd3', 'slb2',
+        or 'slb3'.  Alternatively, you can pass a user defined
+        class which derives from the equation_of_state base class.
+        """
+        if( isinstance(method, basestring)):
+            if (method == "slb2"):
+                self.method = slb.SLB2()
+            elif (method == "mgd2"):
+                self.method = mgd.MGD2()
+            elif (method == "mgd3"):
+                self.method = mgd.MGD3()
+            elif (method == "slb3"):
+                self.method = slb.SLB3()
+            elif (method == "bm2"):
+                self.method = bm.BM2()
+            elif (method == "bm3"):
+                self.method = bm.BM3()
+            else:
+                raise Exception("unsupported material method " + method)
+        elif ( issubclass(method, eos.EquationOfState) ):
+            self.method = method()
+        else:
+            raise Exception("unsupported material method " + method.__class__.__name__ )
+
+    def to_string(self):
+        """
+        Returns the name of the mineral class
+        """
+        return "'" + self.__class__.__module__.replace(".minlib_",".") + "." + self.__class__.__name__ + "'"
+
+    def unroll(self):
+        return ([1.0],[self])
+
+    def set_state(self, pressure, temperature):
+        """
+        Update the material to the given pressure [Pa] and temperature [K].
+
+        This updates the other properties of this class (v_s, v_p, ...).
+        """
+
+        #in an effort to avoid additional work, don't do all the calculations if nothing has changed
+        try:
+            if self.pressure == pressure and self.temperature == temperature and self.old_params == self.params:
+                return
+        except AttributeError:
+            pass  #do nothing
+
+        self.pressure = pressure
+        self.temperature = temperature
+        self.old_params = self.params
+
+        self.V = self.method.volume(self.pressure, self.temperature, self.params)
+        self.gr = self.method.grueneisen_parameter(self.pressure, self.temperature, self.V, self.params)
+        self.K_T = self.method.isothermal_bulk_modulus(self.pressure, self.temperature, self.V, self.params)
+        self.K_S = self.method.adiabatic_bulk_modulus(self.pressure, self.temperature, self.V, self.params)
+        self.C_v = self.method.heat_capacity_v(self.pressure, self.temperature, self.V, self.params)
+        self.C_p = self.method.heat_capacity_p(self.pressure, self.temperature, self.V, self.params)
+        self.alpha = self.method.thermal_expansivity(self.pressure, self.temperature, self.V, self.params)
+
+        if (self.params.has_key('G_0') and self.params.has_key('Gprime_0')):
+            self.G = self.method.shear_modulus(self.pressure, self.temperature, self.V, self.params)
+        else:
+            self.G = float('nan') #nan if there is no G, this should propagate through calculations to the end
+            warnings.warn(('Warning: G_0 and or Gprime_0 are undefined for ' + self.to_string()))
+
+    def molar_mass(self):
+        """
+        Returns molar mass of the mineral [kg/mol]
+        """
+        return self.params['molar_mass']
+
+    def density(self):
+        """
+        Returns density of the mineral [kg/m^3]
+        """
+        return  self.params['molar_mass'] / self.V
+
+    def molar_volume(self):
+        """
+        Returns molar volume of the mineral [m^3/mol]
+        """
+        return self.V
+    def grueneisen_parameter(self):
+        """
+        Returns grueneisen parameter of the mineral [unitless]
+        """
+        return self.gr
+    def isothermal_bulk_modulus(self):
+        """
+        Returns isothermal bulk modulus of the mineral [Pa]
+        """
+        return self.K_T
+    def adiabatic_bulk_modulus(self):
+        """
+        Returns adiabatic bulk modulus of the mineral [Pa]
+        """
+        return self.K_S
+    def shear_modulus(self):
+        """
+        Returns shear modulus of the mineral [Pa]
+        """
+        return self.G
+    def thermal_expansivity(self):
+        """
+        Returns thermal expansion coefficient of the mineral [1/K]
+        """
+        return self.alpha
+    def heat_capacity_v(self):
+        """
+        Returns heat capacity at constant volume of the mineral [J/K/mol]
+        """
+        return self.C_v
+    def heat_capacity_p(self):
+        """
+        Returns heat capacity at constant pressure of the mineral [J/K/mol]
+        """
+        return self.C_p
+    def v_s(self):
+        """
+        Returns shear wave speed of the mineral [m/s]
+        """
+        return np.sqrt(self.shear_modulus() / \
+            self.density())
+    def v_p(self):
+        """
+        Returns P wave speed of the mineral [m/s]
+        """
+        return np.sqrt((self.adiabatic_bulk_modulus() + 4. / 3. * \
+            self.shear_modulus()) / self.density())
+    def v_phi(self):
+        """
+        Returns bulk sound speed of the mineral [m/s]
+        """
+        return np.sqrt(self.adiabatic_bulk_modulus() / self.density())
