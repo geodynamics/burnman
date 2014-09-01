9a6bc82eefc3506f341b1ab42eebea51eee712ab
Bob Myhill <myhill.bob@gmail.com>
2014-09-01 22:46:34 +0200

    Solid solutions in SLB2011 given full names and abbreviated aliases

--- burnman/minerals/SLB_2011_w_solid_solutions.py
+++ burnman/minerals/SLB_2011_w_solid_solutions.py
@@ -16,13 +16,13 @@ from burnman.processchemistry import ProcessChemistry
 from burnman.mineral import Mineral
 from burnman.solidsolution import SolidSolution
 
-class c2c(SolidSolution):
+class c2c_pyroxene(SolidSolution):
     def __init__(self):
         # Name
         self.name='C2/c pyroxene'
 
         # Endmembers (C2/c is symmetric)
-        base_material = [[mgc2(), '[Mg]2Si2O6'],[fec2(), '[Fe]2Si2O6']]
+        base_material = [[hp_clinoenstatite(), '[Mg]2Si2O6'],[hp_clinoferrosilite(), '[Fe]2Si2O6']]
 
         # Interaction parameters (C2c is ideal)
         interaction_parameter=[]
@@ -31,178 +31,178 @@ class c2c(SolidSolution):
 
 
 
-class cf(SolidSolution):
+class ca_ferrite_structured_phase(SolidSolution):
     def __init__(self):
         # Name
-        self.name='calcium ferrite'
+        self.name='calcium ferrite structured phase'
 
         # Endmembers (CF is symmetric)
-        base_material = [[mgcf(), '[Mg]Al[Al]O4'],[fecf(), '[Fe]Al[Al]O4'],[nacf(), '[Na]Al[Si]O4']]
+        base_material = [[mg_calcium_ferrite(), '[Mg]Al[Al]O4'],[fe_calcium_ferrite(), '[Fe]Al[Al]O4'],[na_calcium_ferrite(), '[Na]Al[Si]O4']]
 
         # Interaction parameters (CF is ideal)
         interaction_parameter=[]
 
         SolidSolution.__init__(self, base_material, interaction_parameter)
 
-class cpv(SolidSolution):
+class ca_rich_perovskite(SolidSolution):
     def __init__(self):
         # Name
         self.name='calcium perovskite'
 
         # Endmembers (cpv is symmetric)
-        base_material = [[capv(), '[Ca][Ca]Si2O6'],[jdpv(), '[Na][Al]Si2O6']]
+        base_material = [[ca_perovskite(), '[Ca][Ca]Si2O6'],[jadeitic_perovskite(), '[Na][Al]Si2O6']]
 
         # Interaction parameters (cpv is ideal)
         interaction_parameter=[]
 
         SolidSolution.__init__(self, base_material, interaction_parameter)
 
-class cpx(SolidSolution):
+class clinopyroxene(SolidSolution):
     def __init__(self):
         # Name
         self.name='clinopyroxene'
 
         # Endmembers (cpx is symmetric)
-        base_material = [[di(), '[Ca][Mg][Si]2O6'],[he(), '[Ca][Fe][Si]2O6'],[cen(), '[Mg][Mg][Si]2O6'],[cats(), '[Ca][Al][Si1/2Al1/2]2O6'],[jd(), '[Na][Al][Si]2O6']]
+        base_material = [[diopside(), '[Ca][Mg][Si]2O6'],[hedenbergite(), '[Ca][Fe][Si]2O6'],[clinoenstatite(), '[Mg][Mg][Si]2O6'],[ca_tschermaks_molecule(), '[Ca][Al][Si1/2Al1/2]2O6'],[jadeite(), '[Na][Al][Si]2O6']]
 
         # Interaction parameters
         interaction_parameter=[[0., 24.74e3, 26.e3, 24.3e3],[24.74e3, 0., 0.e3], [60.53136e3, 0.0], [10.e3]]
 
         SolidSolution.__init__(self, base_material, interaction_parameter)
 
-class gt(SolidSolution):
+class garnet(SolidSolution):
     def __init__(self):
         # Name
         self.name='garnet'
 
         # Endmembers (garnet is symmetric)
-        base_material = [[py(), '[Mg]3[Al][Al]Si3O12'],[al(), '[Fe]3[Al][Al]Si3O12'],[gr(), '[Ca]3[Al][Al]Si3O12'],[mgmj(), '[Mg]3[Mg][Si]Si3O12'],[jdmj(), '[Na2/3Al1/3]3[Al][Si]Si3O12']]
+        base_material = [[pyrope(), '[Mg]3[Al][Al]Si3O12'],[almandine(), '[Fe]3[Al][Al]Si3O12'],[grossular(), '[Ca]3[Al][Al]Si3O12'],[mg_majorite(), '[Mg]3[Mg][Si]Si3O12'],[jadeitic_majorite(), '[Na2/3Al1/3]3[Al][Si]Si3O12']]
         # Interaction parameters
         interaction_parameter=[[0.0, 30.e3, 21.20278e3, 0.0],[0.0,0.0,0.0],[57.77596e3, 0.0],[0.0]]
 
         SolidSolution.__init__(self, base_material, interaction_parameter)
 
 
-class il(SolidSolution):
+class akimotoite(SolidSolution):
     def __init__(self):
         # Name
-        self.name='ilmenite/akimotoite'
+        self.name='akimotoite/ilmenite'
 
         # Endmembers (ilmenite/akimotoite is symmetric)
-        base_material = [[mgil(), '[Mg][Si]O3'],[feil(), '[Fe][Si]O3'],[co(), '[Al][Al]O3']]
+        base_material = [[mg_akimotoite(), '[Mg][Si]O3'],[fe_akimotoite(), '[Fe][Si]O3'],[corundum(), '[Al][Al]O3']]
         # Interaction parameters
         interaction_parameter=[[0.0, 66.e3],[66.e3]]
 
         SolidSolution.__init__(self, base_material, interaction_parameter)
 
-class mw(SolidSolution):
+class ferropericlase(SolidSolution):
     def __init__(self):
         # Name
         self.name='magnesiowustite/ferropericlase'
 
         # Endmembers (ferropericlase is symmetric)
-        base_material = [[pe(), '[Mg]O'],[wu(), '[Fe]O']]
+        base_material = [[periclase(), '[Mg]O'],[wuestite(), '[Fe]O']]
         # Interaction parameters
         interaction_parameter=[[13.e3]]
 
         SolidSolution.__init__(self, base_material, interaction_parameter)
 
-class ol(SolidSolution):
+class mg_fe_olivine(SolidSolution):
     def __init__(self):
         # Name
         self.name='olivine'
 
         # Endmembers (olivine is symmetric)
-        base_material = [[fo(), '[Mg]2SiO4'],[wu(), '[Fe]2SiO4']]
+        base_material = [[forsterite(), '[Mg]2SiO4'],[fayalite(), '[Fe]2SiO4']]
         # Interaction parameters
         interaction_parameter=[[7.81322e3]]
 
         SolidSolution.__init__(self, base_material, interaction_parameter)
 
-class opx(SolidSolution):
+class orthopyroxene(SolidSolution):
     def __init__(self):
         # Name
         self.name='orthopyroxene'
 
         # Endmembers (orthopyroxene is symmetric)
-        base_material = [[en(), '[Mg][Mg][Si]SiO6'],[fs(), '[Fe][Fe][Si]SiO6'],[mgts(), '[Mg][Al][Al]SiO6'],[odi(), '[Ca][Mg][Si]SiO6']]
+        base_material = [[enstatite(), '[Mg][Mg][Si]SiO6'],[ferrosilite(), '[Fe][Fe][Si]SiO6'],[mg_tschermaks_molecule(), '[Mg][Al][Al]SiO6'],[orthodiopside(), '[Ca][Mg][Si]SiO6']]
 
         # Interaction parameters
         interaction_parameter=[[0.0, 0.0, 32.11352e3],[0.0, 0.0],[48.35316e3]]
 
         SolidSolution.__init__(self, base_material, interaction_parameter)
 
-class plag(SolidSolution):
+class plagioclase(SolidSolution):
     def __init__(self):
         # Name
         self.name='plagioclase'
 
         # Endmembers (plagioclase is symmetric)
-        base_material = [[an(), '[Ca][Al]2Si2O8'],[ab(), '[Na][Al1/2Si1/2]2Si2O8']]
+        base_material = [[anorthite(), '[Ca][Al]2Si2O8'],[albite(), '[Na][Al1/2Si1/2]2Si2O8']]
         # Interaction parameters
         interaction_parameter=[[26.0e3]]
 
         SolidSolution.__init__(self, base_material, interaction_parameter)
 
-class ppv(SolidSolution):
+class post_perovskite(SolidSolution):
     def __init__(self):
         # Name
         self.name='post-perovskite/bridgmanite'
 
         # Endmembers (post perovskite is symmetric)
-        base_material = [[mppv(), '[Mg][Si]O3'],[fppv(), '[Fe][Si]O3'],[appv(), '[Al][Al]O3']]
+        base_material = [[mg_post_perovskite(), '[Mg][Si]O3'],[fe_post_perovskite(), '[Fe][Si]O3'],[al_post_perovskite(), '[Al][Al]O3']]
 
         # Interaction parameters
         interaction_parameter=[[0.0, 60.0e3],[0.0]]
 
         SolidSolution.__init__(self, base_material, interaction_parameter)
 
-class pv(SolidSolution):
+class mg_fe_perovskite(SolidSolution):
     def __init__(self):
         # Name
-        self.name='perovskite/bridgmanite'
+        self.name='magnesium silicate perovskite/bridgmanite'
 
         # Endmembers (post perovskite is symmetric)
-        base_material = [[mgpv(), '[Mg][Si]O3'],[fepv(), '[Fe][Si]O3'],[alpv(), '[Al][Al]O3']]
+        base_material = [[mg_perovskite(), '[Mg][Si]O3'],[fe_perovskite(), '[Fe][Si]O3'],[al_perovskite(), '[Al][Al]O3']]
 
         # Interaction parameters
         interaction_parameter=[[0.0, 116.0e3],[0.0]]
 
         SolidSolution.__init__(self, base_material, interaction_parameter)
 
-class ri(SolidSolution):
+class mg_fe_ringwoodite(SolidSolution):
     def __init__(self):
         # Name
         self.name='ringwoodite'
 
         # Endmembers (post perovskite is symmetric)
-        base_material = [[mgri(), '[Mg]2SiO4'],[feri(), '[Fe]2SiO4']]
+        base_material = [[mg_ringwoodite(), '[Mg]2SiO4'],[fe_ringwoodite(), '[Fe]2SiO4']]
 
         # Interaction parameters
         interaction_parameter=[[9.34084e3]]
 
         SolidSolution.__init__(self, base_material, interaction_parameter)
 
-class mg_fe_spinel(SolidSolution):
+class mg_fe_aluminous_spinel(SolidSolution):
     def __init__(self):
         # Name
-        self.name='spinel, fixed order'
+        self.name='spinel-hercynite binary, fixed order'
 
         # Endmembers (post perovskite is symmetric)
-        base_material = [[sp(), '[Mg3/4Al1/4]4[Al7/8Mg1/8]8O16'],[hc(), '[Fe3/4Al1/4]4[Al7/8Fe1/8]8O16']]
+        base_material = [[spinel(), '[Mg3/4Al1/4]4[Al7/8Mg1/8]8O16'],[hercynite(), '[Fe3/4Al1/4]4[Al7/8Fe1/8]8O16']]
 
         # Interaction parameters
         interaction_parameter=[[5.87646e3]]
 
         SolidSolution.__init__(self, base_material, interaction_parameter)
 
-class wa(SolidSolution):
+class mg_fe_wadsleyite(SolidSolution):
     def __init__(self):
         # Name
         self.name='wadsleyite'
 
         # Endmembers (post perovskite is symmetric)
-        base_material = [[mgwa(), '[Mg]2SiO4'],[fewa(), '[Fe]2SiO4']]
+        base_material = [[mg_wadsleyite(), '[Mg]2SiO4'],[fe_wadsleyite(), '[Fe]2SiO4']]
 
         # Interaction parameters
         interaction_parameter=[[16.74718e3]]
@@ -343,7 +343,71 @@ class fe_perovskite(Mineral):
             'err_q_0':1.0,
             'err_eta_s_0':1.0}
 
+# Mineral aliases
+mgc2 = hp_clinoenstatite
+fec2 = hp_clinoferrosilite
+mgcf = mg_calcium_ferrite
+fecf = fe_calcium_ferrite
+nacf = na_calcium_ferrite
+capv = calcium_perovskite
+jdpv = jadeitic_perovskite
+di = diopside
+he = hedenbergite
+cen = clinoenstatite
+cats = ca_tschermaks_molecule
+jd = jadeite
+py = pyrope
+al = almandine
+gr = grossular
+mgmj = mg_majorite
+jdmj = jadeitic_majorite
+mgil = mg_akimotoite
+feil = fe_akimotoite
+co = corundum
+pe = periclase
+wu = wuestite
+fo = forsterite
+fa = fayalite
+en = enstatite
+fs = ferrosilite
+mgts = mg_tschermaks_molecule
+odi = orthodiopside
+an = anorthite
+ab = albite
+mppv = mg_post_perovskite
+fppv = fe_post_perovskite
+appv = al_post_perovskite
+mgpv = mg_perovskite
 mg_bridgmanite = mg_perovskite
+fepv = fe_perovskite
 fe_bridgmanite = fe_perovskite
+alpv = al_perovskite
+mgri = mg_ringwoodite
+feri = fe_ringwoodite
+sp = spinel
+hc = hercynite
+mgwa = mg_wadsleyite
+fewa = fe_wadsleyite
+
+
+# Solid solution aliases
+c2c = c2c_pyroxene
+cf = calcium_ferrite_structured_phase
+cpv = ca_rich_perovskite
+cpx = clinopyroxene
+gt = garnet
+il = akimotoite
+ilmenite_group = akimotoite
+mw = ferropericlase
+magnesiowuestite = ferropericlase
+ol = mg_fe_olivine
+opx = orthopyroxene
+plag = plagioclase
+ppv = post_perovskite
+pv = mg_fe_perovskite
 mg_fe_bridgmanite = mg_fe_perovskite
-mg_fe_bridgmanite_pt_dependent = mg_fe_perovskite_pt_dependent
+mg_fe_silicate_perovskite = mg_fe_perovskite
+ri = mg_fe_ringwoodite
+spinel_group=mg_fe_aluminous_spinel
+wa = mg_fe_wadsleyite
+spinelloid_III = mg_fe_wadsleyite
