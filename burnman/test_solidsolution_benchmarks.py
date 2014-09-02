63282475a8b423f338167451fdf51c4ee039a32d
Bob Myhill <myhill.bob@gmail.com>
2014-09-02 23:12:40 +0200

    Started benchmarking document to test solid solutions

---
+++ burnman/test_solidsolution_benchmarks.py
@@ -0,0 +1,27 @@
+# Benchmarks for the solid solution class
+
+'''
+Solvus shapes (a proxy for Gibbs free energy checking
+'''
+
+# van Laar parameter
+# Figure 2a of Holland and Powell, 2003
+
+# Temperature dependence
+# Figure 2b of Holland and Powell, 2003
+
+# A specific solvus example: sanidine-high albite
+# Includes asymmetry and pressure, temperature dependence
+# Figure 3 of Holland and Powell, 2003
+
+
+'''
+Excess properties
+'''
+# Configurational entropy
+# Figure 3b of Stixrude and Lithgow-Bertelloni, 2011
+
+# Excess enthalpy of solution
+# Figure 5 of Stixrude and Lithgow-Bertelloni, 2011
+
+# Excess volume of solution
