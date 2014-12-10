8fcb45ffaeccedd6294937a765be7f89b818b41e
I <kmanunterborn@gmail.com>
2014-12-09 21:54:50 -0800

    Typo
    Typo

--- burnman/tools.py
+++ burnman/tools.py
@@ -1,4 +1,4 @@
-l# BurnMan - a lower mantle toolkit
+# BurnMan - a lower mantle toolkit
 # Copyright (C) 2012, 2013, Heister, T., Unterborn, C., Rose, I. and Cottaar, S.
 # Released under GPL v2 or later.
 
