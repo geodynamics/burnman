6459255d2ca12c6681120b3d6c6ad902475aa9ee
I <kmanunterborn@gmail.com>
2014-12-11 21:34:35 -0800

    Fixed tabbing
    Tabs to spaces

--- tests/test_eos.py
+++ tests/test_eos.py
@@ -38,8 +38,8 @@ class eos(BurnManTest):
 		eoses = [burnman.slb.SLB2(),burnman.slb.SLB3(),burnman.birch_murnaghan.BM2(),burnman.birch_murnaghan.BM3()]
 		
 		for i in eoses:
-			Volume_test = i.volume(pressure, temperature,rock.params)
-			self.assertFloatEqual(Volume_test,rock.params['V_0'])
+            Volume_test = i.volume(pressure, temperature,rock.params)
+            self.assertFloatEqual(Volume_test,rock.params['V_0'])
         	K_test = i.adiabatic_bulk_modulus(pressure, temperature,rock.params['V_0'],rock.params)
         	self.assertFloatEqual(K_test,rock.params['K_0'])
         	G_test = i.shear_modulus(pressure, temperature,rock.params['V_0'],rock.params)
@@ -58,7 +58,7 @@ class eos(BurnManTest):
 
 		eoses_thermal = [burnman.slb.SLB2(),burnman.slb.SLB3()]
 		for i in eoses_thermal:
-			Cp_test= i.heat_capacity_p(pressure, temperature,rock.params['V_0'],rock.params)
+            Cp_test= i.heat_capacity_p(pressure, temperature,rock.params['V_0'],rock.params)
         	self.assertFloatEqual(Cp_test,37.076768469502042)
         	Cv_test= i.heat_capacity_v(pressure, temperature,rock.params['V_0'],rock.params)
         	self.assertFloatEqual(Cv_test,36.577717628901553)        
