64485e8758fd13db83ccb3664a8294f6c32099f1
Bob Myhill <myhill.bob@gmail.com>
2014-12-11 21:39:56 -0800

    Create new endmember and solid solution databases

--- burnman/minerals/__init__.py
+++ burnman/minerals/__init__.py
@@ -5,19 +5,34 @@
 """
 Mineral database
 
-  - :mod:`~burnman.minerals.Matas_etal_2007`
-  - :mod:`~burnman.minerals.Murakami_2013`
-  - :mod:`~burnman.minerals.Murakami_etal_2012`
   - :mod:`~burnman.minerals.SLB_2005`
   - :mod:`~burnman.minerals.SLB_2011_ZSB_2013`
   - :mod:`~burnman.minerals.SLB_2011`
+  - :mod:`~burnman.minerals.Murakami_etal_2012`
+  - :mod:`~burnman.minerals.Murakami_2013`
+  - :mod:`~burnman.minerals.Matas_etal_2007`
+  - :mod:`~burnman.minerals.HP_2011_fluids`
+  - :mod:`~burnman.minerals.HP_2011_ds62`
+  - :mod:`~burnman.minerals.HHPH_2013`
   - :mod:`~burnman.minerals.other`
 """
 
-import Murakami_etal_2012
-import Matas_etal_2007
+# Stixrude and Lithgow-Bertelloni
 import SLB_2011
 import SLB_2011_ZSB_2013
 import SLB_2005
+
+# Murakami and coworkers
+import Murakami_etal_2012
 import Murakami_2013
+
+# Matas and coworkers
+import Matas_etal_2007
+
+# Holland, Powell and coworkers
+import HP_2011_fluids
+import HP_2011_ds62
+import HHPH_2013
+
+# Other
 import other
