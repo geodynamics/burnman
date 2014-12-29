8afad1dc5de8b88e444481436d39aa4c40cefb00
ian-r-rose <ian.r.rose@gmail.com>
2014-12-28 22:21:45 -0800

    Check for zero shear modulus

--- burnman/solidsolution.py
+++ burnman/solidsolution.py
@@ -86,7 +86,12 @@ class SolidSolution(Mineral):
         self.C_p = sum([ self.base_material[i][0].C_p * self.molar_fraction[i] for i in range(self.n_endmembers) ])
         self.alpha = (1./self.V) * sum([ self.base_material[i][0].alpha * self.base_material[i][0].V * self.molar_fraction[i] for i in range(self.n_endmembers) ])
         self.K_T = self.V * 1./(sum([ self.base_material[i][0].V / (self.base_material[i][0].K_T)  * self.molar_fraction[i] for i in range(self.n_endmembers) ]))
-        self.G = self.V * 1./(sum([ self.base_material[i][0].V / (self.base_material[i][0].G)  * self.molar_fraction[i] for i in range(self.n_endmembers) ]))
+ 
+        G_list = [ self.base_material[i][0].G for i in range(self.n_endmembers) ]
+        if 0.0 in G_list:
+            self.G = 0.0
+        else:
+            self.G = self.V * 1./(sum([ self.base_material[i][0].V / (self.base_material[i][0].G)  * self.molar_fraction[i] for i in range(self.n_endmembers) ]))
 
         # Derived properties
         self.C_v = self.C_p - self.V*temperature*self.alpha*self.alpha*self.K_T
