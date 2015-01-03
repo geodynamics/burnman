d124c5a6fd08f6e725ee5f08292c1d5d835d9cfa
ian-r-rose <ian.r.rose@gmail.com>
2015-01-02 23:44:00 -0800

    Add a bit on MGD

--- burnman/eos/mie_grueneisen_debye.py
+++ burnman/eos/mie_grueneisen_debye.py
@@ -13,10 +13,12 @@ import burnman.constants as constants
 
 class MGDBase(eos.EquationOfState):
     """
-    Base class for a generic finite-strain-mie-grueneisen-debye
+    Base class for a generic finite-strain Mie-Grueneisen-Debye
     equation of state.  References for this can be found in many
-    places, such as Shim, Duffym and Kenichi (2002) and Jackson and Rigedn
-    (1996).  Here we mostly follow the appendices of Matas et al (2007)
+    places, such as Shim, Duffym and Kenichi (2002) and Jackson and Rigden
+    (1996).  Here we mostly follow the appendices of Matas et al (2007).
+    Of particular note is the thermal correction to the shear modulus, which
+    was developed by Hama and Suito (1998).
     """
 
     #def __init__(self):
