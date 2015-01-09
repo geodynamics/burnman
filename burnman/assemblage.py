667841f5a04795883b06040f74ba779f9678b70c
Bob Myhill <myhill.bob@gmail.com>
2015-01-09 13:40:03 +0100

    Removed tabs

--- burnman/assemblage.py
+++ burnman/assemblage.py
@@ -44,7 +44,7 @@ class Assemblage(Material):
             list of phases.
         """
         assert(len(phases)>0)
-	self.phases=phases
+        self.phases=phases
 
 
     def debug_print(self, indent=""):
@@ -85,7 +85,7 @@ class Assemblage(Material):
 
         for idx, phase in enumerate(self.phases):
             fraction=self.fractions[idx]
-	    p_fr,p_min = phase.unroll()
+            p_fr,p_min = phase.unroll()
             check_pairs(p_fr,p_min)
             fractions.extend([i*fraction for i in p_fr])
             minerals.extend(p_min)
@@ -116,15 +116,15 @@ class Assemblage(Material):
 
     def calculate_moduli(self):
         """
-	Calculate the elastic moduli and densities of the individual phases in the assemblage.
+        Calculate the elastic moduli and densities of the individual phases in the assemblage.
 
-	:returns:
-	answer -- an array of (n_evaluation_points by n_phases) of
-	elastic_properties(), so the result is of the form
-	answer[pressure_idx][phase_idx].V
-	:rtype: list of list of :class:`burnman.elastic_properties`
-	"""
-	elastic_properties_of_phases = []
+        :returns:
+        answer -- an array of (n_evaluation_points by n_phases) of
+        elastic_properties(), so the result is of the form
+        answer[pressure_idx][phase_idx].V
+        :rtype: list of list of :class:`burnman.elastic_properties`
+        """
+        elastic_properties_of_phases = []
         (fractions,minerals) = rock.unroll()
         for (fraction,mineral) in zip(fractions,minerals):
             e = ElasticProperties()
@@ -139,38 +139,38 @@ class Assemblage(Material):
 
     def assemblage_thermodynamic_properties(self):
         """
-	Calculates the thermodynamic properties of the assemblage
-	:returns: :math:`\mathcal{G}` :math:`[J/mol]`, :math:`\mathcal{H}` :math:`[J/mol]` , :math:`\mathcal{S}` :math:`[J/K/mol]`, :math:`V` :math:`[m^3/mol]`, :math:`C_p` :math:`[J/K/mol]`, :math:`C_v` :math:`[J/K/mol]`, :math:`alpha` :math:`[1/K]`, :math:`\gamma` :math:`[]`,
-	:rtype: lists of floats
-	"""
-	thermodynamic_properties_of_phases = []
+        Calculates the thermodynamic properties of the assemblage
+        :returns: :math:`\mathcal{G}` :math:`[J/mol]`, :math:`\mathcal{H}` :math:`[J/mol]` , :math:`\mathcal{S}` :math:`[J/K/mol]`, :math:`V` :math:`[m^3/mol]`, :math:`C_p` :math:`[J/K/mol]`, :math:`C_v` :math:`[J/K/mol]`, :math:`alpha` :math:`[1/K]`, :math:`\gamma` :math:`[]`,
+        :rtype: lists of floats
+        """
+        thermodynamic_properties_of_phases = []
         (fractions,minerals) = rock.unroll()
-	gibbs = sum([ minerals[i].gibbs * fractions[i] for i in range(minerals) ])
-	H = sum([ minerals[i].H * fractions[i] for i in range(minerals) ])
-	S = sum([ minerals[i].S * fractions[i] for i in range(minerals) ])
-	V = sum([ minerals[i].V * fractions[i] for i in range(minerals) ])
+        gibbs = sum([ minerals[i].gibbs * fractions[i] for i in range(minerals) ])
+        H = sum([ minerals[i].H * fractions[i] for i in range(minerals) ])
+        S = sum([ minerals[i].S * fractions[i] for i in range(minerals) ])
+        V = sum([ minerals[i].V * fractions[i] for i in range(minerals) ])
         #C_p = sum([ minerals[i].C_p * fractions[i] for i in range(minerals) ])
         #C_v = sum([ minerals[i].C_v * fractions[i] for i in range(minerals) ])
         #alpha = sum([ minerals[i].alpha * fractions[i] for i in range(minerals) ])
         #gr = sum([ minerals[i].gr * fractions[i] for i in range(minerals) ])
-	return gibbs, H, S, V #, C_p, C_v, alpha, gr
+        return gibbs, H, S, V #, C_p, C_v, alpha, gr
 
     def assemblage_elastic_properties(self, averaging_scheme=burnman.averaging_schemes.VoigtReussHill()):
         """
-	Calculates the seismic velocities of the Assemblage, using
-	an averaging scheme for the velocities of individual phases
+        Calculates the seismic velocities of the Assemblage, using
+        an averaging scheme for the velocities of individual phases
 
-	:type averaging_scheme: :class:`burnman.averaging_schemes.averaging_scheme`
-	:param averaging_scheme: Averaging scheme to use.
+        :type averaging_scheme: :class:`burnman.averaging_schemes.averaging_scheme`
+        :param averaging_scheme: Averaging scheme to use.
 
-	:returns: :math:`\\rho` :math:`[kg/m^3]` , :math:`V_p, V_s,` and :math:`V_{\phi}` :math:`[m/s]`, bulk modulus :math:`K` :math:`[Pa]`,shear modulus :math:`G` :math:`[Pa]`
-	:rtype: lists of floats
+        :returns: :math:`\\rho` :math:`[kg/m^3]` , :math:`V_p, V_s,` and :math:`V_{\phi}` :math:`[m/s]`, bulk modulus :math:`K` :math:`[Pa]`,shear modulus :math:`G` :math:`[Pa]`
+        :rtype: lists of floats
 
-	"""
-	moduli_list = self.calculate_moduli()
-	moduli = average_moduli(moduli_list, averaging_scheme)
-	mat_vp, mat_vs, mat_vphi = compute_velocities(moduli)
-	mat_rho = np.array([m.rho for m in moduli])
-	mat_K = np.array([m.K for m in moduli])
-	mat_G = np.array([m.G for m in moduli])
-	return mat_rho, mat_vp, mat_vs, mat_vphi, mat_K, mat_G
+        """
+        moduli_list = self.calculate_moduli()
+        moduli = average_moduli(moduli_list, averaging_scheme)
+        mat_vp, mat_vs, mat_vphi = compute_velocities(moduli)
+        mat_rho = np.array([m.rho for m in moduli])
+        mat_K = np.array([m.K for m in moduli])
+        mat_G = np.array([m.G for m in moduli])
+        return mat_rho, mat_vp, mat_vs, mat_vphi, mat_K, mat_G
