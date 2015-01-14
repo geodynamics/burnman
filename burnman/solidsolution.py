a3e51efde6bded970152afd1b6001e06ad38281a
sannecottaar <sanne.cottaar@gmail.com>
2015-01-14 13:18:04 +0000

    fix burnman/solidsolution.py

--- burnman/solidsolution.py
+++ burnman/solidsolution.py
@@ -108,13 +108,6 @@ class SolidSolution(Mineral):
         molar_mass = sum([ self.endmembers[i][0].molar_mass()*self.molar_fractions[i] for i in range(self.n_endmembers) ])
         return molar_mass
 
-    def molar_mass(self):
-        """
-        Returns molar mass of the mineral [kg/mol]
-        """
-        molar_mass = sum([ self.base_material[i][0].molar_mass()*self.molar_fraction[i] for i in range(self.n_endmembers) ])
-        return molar_mass
-
     def set_state(self, pressure, temperature):
         self.pressure=pressure
         self.temperature=temperature
