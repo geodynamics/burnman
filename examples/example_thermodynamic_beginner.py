9d7132d916ef2cd8e77747ef3c7c2dced8a093a7
Bob Myhill <myhill.bob@gmail.com>
2015-08-27 00:41:28 +0200

    Test P_ref

--- examples/example_thermodynamic_beginner.py
+++ examples/example_thermodynamic_beginner.py
@@ -23,6 +23,7 @@ thermodynamics.
 """
 
 import os, sys, numpy as np, matplotlib.pyplot as plt
+from scipy import optimize
 #hack to allow scripts to be placed in subdirectories next to burnman:
 if not os.path.exists('burnman') and os.path.exists('../burnman'):
     sys.path.insert(1,os.path.abspath('..'))
@@ -31,46 +32,114 @@ import burnman
 from burnman import minerals
 import matplotlib.pyplot as plt
 
+T_ref = 1273.
+P_ref = 49.9999999e9
+
+T_obs = 1273.
+P_obs = 100.e9
+
+
 fo=minerals.HP_2011_ds62.fo()
-fo.params['P_0'] = 0.99999999999e5
-room_pressure = 1.e5
-temperatures = np.linspace(50., 1500., 100)
+
+
+def Cp(T, a, b, c, d):
+    return a + b*T + c/T/T + d/np.sqrt(T)
+
+fo.set_state(P_ref, T_ref)
+
+Cps = []
+temperatures = np.linspace(300., 5000., 100)
+for T in temperatures:
+    fo.set_state(P_obs, T)
+    Cps.append(fo.C_p)
+    
+Cp_new = optimize.curve_fit(Cp, temperatures, Cps)[0]
+a, b, c, d = Cp_new
+
+
+fo.set_state(P_obs, T_obs)
+print fo.gibbs, fo.S, fo.C_p
+
+
+
+Cp0 = []
+temperatures = np.linspace(300., 5000., 100)
+for T in temperatures:
+    fo.set_state(P_obs, T)
+    Cp0.append(fo.C_p)
+
+
+temperatures = np.linspace(300., 5000., 100)
 alphas_0 = []
 volumes_0 = []
+gibbs_0 = []
 for T in temperatures:
-    fo.set_state(room_pressure, T)
+    fo.set_state(P_obs, T)
     alphas_0.append(fo.alpha)
     volumes_0.append(fo.V)
+    gibbs_0.append(fo.gibbs)
 
-T_eos = 2273.
-pressures = np.linspace(1.e5, 50.e9, 100)
+
+pressures = np.linspace(1.e5, 100.e9, 100)
 eos_0 = []
 for P in pressures:
-    fo.set_state(P, T_eos)
+    fo.set_state(P, T_obs)
     eos_0.append(fo.V)
 
 
-T_ref = 1273.
-fo.set_state(room_pressure, T_ref)
+dP = 100000.
+fo.set_state(P_ref-dP, T_ref)
+
+K0 = fo.K_T 
+fo.set_state(P_ref, T_ref)
+K1 = fo.K_T
+fo.set_state(P_ref+dP, T_ref)
+K2 = fo.K_T
+
+grad0 = (K1 - K0)/dP
+grad1 = (K2 - K1)/dP
+
+
+fo.set_state(P_ref, T_ref)
 fo.params['T_0'] = T_ref
+fo.params['P_0'] = P_ref
 fo.params['H_0'] = fo.H
 fo.params['S_0'] = fo.S
 fo.params['V_0'] = fo.V
+#fo.params['Cp'] = [a, b, c, d]
 fo.params['K_0'] = fo.K_T 
-fo.params['Kdprime_0'] = -fo.params['Kprime_0']/fo.params['K_0']
 fo.params['a_0'] = fo.alpha
+fo.params['Kprime_0'] = (K2 - K0)/(2.*dP)
+fo.params['Kdprime_0'] = (grad1 - grad0)/dP
+
+
+fo.set_state(P_obs, T_obs)
+print fo.gibbs, fo.S, fo.C_p
+
+Cp1 = []
+temperatures = np.linspace(300., 5000., 100)
+for T in temperatures:
+    fo.set_state(P_obs, T)
+    Cp1.append(fo.C_p)
+
+
+plt.plot(temperatures, np.array(Cp1) - np.array(Cp0))
+plt.show()
+
 
 alphas_1 = []
 volumes_1 = []
+gibbs_1 = []
 for T in temperatures:
-    fo.set_state(1.e5, T)
+    fo.set_state(P_obs, T)
     alphas_1.append(fo.alpha)
     volumes_1.append(fo.V)
+    gibbs_1.append(fo.gibbs)
     
 plt.plot(temperatures, np.array(alphas_0), label='Tref = room temperature')
 plt.plot(temperatures, np.array(alphas_1), label='Tref = '+str(T_ref)+' K')
 
-plt.title('Thermal expansion at '+str(room_pressure/1.e9)+' GPa')
+plt.title('Thermal expansion at '+str(P_obs/1.e9)+' GPa')
 plt.legend(loc='lower right')
 plt.xlabel('Temperatures (K)')
 plt.ylabel('Thermal expansivity (m^3/m^3/K)')
@@ -84,17 +153,25 @@ plt.xlabel('Temperatures (K)')
 plt.ylabel('Volumes (m^3/mol)')
 plt.show()
 
-pressures = np.linspace(1.e5, 50.e9, 100)
+pressures = np.linspace(1.e5, 100.e9, 100)
 eos_1 = []
 for P in pressures:
-    fo.set_state(P, T_eos)
+    fo.set_state(P, T_obs)
     eos_1.append(fo.V)
 
 plt.plot(pressures/1.e9, np.array(eos_0), label='Tref = room temperature')
 plt.plot(pressures/1.e9, np.array(eos_1), label='Tref = '+str(T_ref)+' K')
 
-plt.title('Compression at '+str(T_eos)+' K')
+plt.title('Compression at '+str(T_obs)+' K')
 plt.legend(loc='lower right')
 plt.xlabel('Pressures (GPa)')
 plt.ylabel('Volumes (m^3/mol)')
+plt.show()
+
+
+plt.plot(temperatures, np.array(gibbs_0) - np.array(gibbs_1))
+
+plt.title('Compression at '+str(T_obs)+' K')
+plt.xlabel('Temperatures (K)')
+plt.ylabel('diff gibbs')
 plt.show()
