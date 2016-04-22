e9aea98cc4fc70b9bee0318f645b9dfd1ccb4654
Bob Myhill <myhill.bob@gmail.com>
2016-04-22 01:10:10 +0100

    Added lower mantle example

--- examples/example_gibbs_minimization.py
+++ examples/example_gibbs_minimization.py
@@ -49,7 +49,8 @@ sol = gibbs_minimizer(composition, assemblage, constraints)
 P_inv = sol[0]
 T_inv = sol[1]
 
-print('The aluminosilicate invariant can be found at {0:.2f} GPa and {1:.0f} K'.format(P_inv/1.e9, T_inv))
+print('Example 1: The aluminosilicates')
+print('The and-sill-ky invariant can be found at {0:.2f} GPa and {1:.0f} K'.format(P_inv/1.e9, T_inv))
 print()
 
 # Kyanite-sillimanite appears at positive P
@@ -109,11 +110,12 @@ x_ol_inv = ol.molar_fractions[1]
 x_wad_inv = wad.molar_fractions[1]
 x_rw_inv = rw.molar_fractions[1]
 
-
+print('Example 2: The olivine polymorphs')
 print('The pressure of the olivine polymorph invariant at {0:.0f} K is {1:.1f} GPa'.format(T, P_inv/1.e9))
 print('The molar Fe2SiO4 contents of coexisting ol, wad and rw')
 print('at this point are {0:.2f}, {1:.2f} and {2:.2f}'.format(x_ol_inv, x_wad_inv, x_rw_inv))
-
+print()
+    
 plt.plot([x_ol_inv, x_rw_inv], [P_inv/1.e9, P_inv/1.e9])
 
 assemblages = [burnman.Composite([ol, wad]),
@@ -139,6 +141,37 @@ for i, assemblage in enumerate(assemblages):
 plt.title('Mg2SiO4-Fe2SiO4 phase diagram at '+str(T)+' K')
 plt.xlabel('p (Fe2SiO4)')
 plt.ylabel('Pressure (GPa)')
+plt.legend(loc='upper right')
+plt.show()
+
+
+
+# Solve 3: Let's turn our sights to the lower mantle
+bdg = SLB_2011.mg_fe_bridgmanite()
+fper = SLB_2011.ferropericlase()
+
+
+T = 2000.
+composition = { 'Mg': 1.775, 'Fe': 0.2, 'Al': 0.05, 'Si': 0.975, 'O': 4.}
+assemblage = burnman.Composite([bdg, fper])
+
+
+pressures = np.linspace(22.e9, 130.e9, 21)
+x_bdg = np.empty_like(pressures)
+x_fper = np.empty_like(pressures)
+for i, P in enumerate(pressures):
+    sol = gibbs_minimizer(composition, assemblage, [['P', P], ['T', T]])
+    x_bdg[i] = sol[3]
+    x_fper[i] = sol[6]
+
+print('Example 3: Lower mantle phase relations in peridotite')
+print('Composition: {}'.format(composition))
+print('At {0:.1f} GPa and {1:.0f} K, ferropericlase contains {2:.1f} mole percent FeO'.format(pressures[0]/1.e9, T, 100.*x_fper[0]))
+print('At {0:.1f} GPa and {1:.0f} K, this has changed to {2:.1f} mole percent FeO'.format(pressures[-1]/1.e9, T, 100.*x_fper[-1]))
+
+    
+plt.plot(pressures, x_bdg, label='x(Fe) bdg')
+plt.plot(pressures, x_fper, label='x(Fe) fper')
 plt.legend(loc='upper right')
 plt.show()
 
