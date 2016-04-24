f4b5f4b17ed33da5889655c06bee93279218358b
Bob Myhill <myhill.bob@gmail.com>
2016-04-24 21:20:22 +0100

    Updated first example to make use of the new higher level functions

--- examples/example_gibbs_minimization.py
+++ examples/example_gibbs_minimization.py
@@ -30,10 +30,9 @@ from burnman.minerals import HP_2011_ds62, SLB_2011
 import numpy as np
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
-from burnman.equilibriumassemblage import gibbs_minimizer, gibbs_bulk_minimizer
+from burnman.equilibriumassemblage import gibbs_minimizer, gibbs_bulk_minimizer, find_invariant, find_univariant
 
 if __name__ == "__main__":
-
     # Example 1: The classic aluminosilicate diagram.
     # This is a T-P phase diagram with composition Al2SiO5,
     # which is the composition of andalusite, sillimanite and
@@ -42,69 +41,36 @@ if __name__ == "__main__":
     sillimanite = HP_2011_ds62.sill()
     kyanite = HP_2011_ds62.ky()
 
-    # Here we define the composition and the assemblage of interest
-    composition = andalusite.params['formula'] 
-    assemblage = burnman.Composite([andalusite, sillimanite, kyanite])
+    # Here we define the composition of interest
+    composition = andalusite.params['formula']
     
     # First, we need to solve for the invariant point where the
-    # three phases coexist. The gibbs minimizer takes compositional
-    # constraints as a list with the first element being the string 'X'.
-    # The second two elements are the multipliers for the compositional vector,
-    # which has the form amount of phase 0, fraction of endmember 1 in phase 0,
-    # fraction of endmember 2 in phase 0 ... amount of phase i ...
-    # In this case, we are dealing with pure phases, so the compositional
-    # vector is the amount of andalusite, sillimanite and kyanite.
-    # These do not have to sum to one. The compositional contraints are
-    # expressed in the form:
-    # dot(constraint[1],cvector)/dot(constraint[2],cvector) = constraint[3]
-    # For the invariant point, the two constraints are that the proportion
-    # of two of the phases goes to zero.
-    constraints= [['X', [1., 0., 0.], [1., 1., 1.], 0.],
-                  ['X', [0., 1., 0.], [1., 1., 1.], 0.]]
-
-    # Here we call the gibbs minimizer. The output is given as a list, where
-    # pressure and temperature are always the first two elements,
-    # followed by the compositional vector
-    sol = gibbs_minimizer(composition, assemblage, constraints)
-    P_inv = sol['P']
-    T_inv = sol['T']
-
-    # Here we simply print the P-T position of the invariant point
+    # three phases coexist. The function find_invariant takes a bulk composition
+    # and then two variables which constitute the stable phases and the two phases
+    # which become stable at that point.
+    P_inv, T_inv = find_invariant(composition, [andalusite], [sillimanite, kyanite])
+    
+    # Here we print the P-T position of the invariant point
     print('Example 1: The aluminosilicates')
     print('The and-sill-ky invariant can be found at {0:.2f} GPa and {1:.0f} K'.format(P_inv/1.e9, T_inv))
     print()
 
-    # Finally, let's plot the full phase diagram
+    # Now let's plot the full phase diagram
     # The kyanite-sillimanite reaction line appears at pressures above the invariant point
     lo_pressures = np.linspace(1.e5, P_inv, 21)
     hi_pressures = np.linspace(P_inv, 1.e9, 21)
 
-    and_ky_temperatures = np.empty_like(lo_pressures)
-    and_sill_temperatures = np.empty_like(lo_pressures)
-    sill_ky_temperatures = np.empty_like(hi_pressures)
+    # For isochemical diagrams, we can make use of the function find_univariant,
+    # which for a given composition and a pressure (temperature) range outputs the temperatures
+    # (pressures) where one phase in an assemblage becomes unstable.
+    and_ky_temperatures = find_univariant(composition, [andalusite], kyanite, 'P', lo_pressures)
+    and_sill_temperatures = find_univariant(composition, [andalusite], sillimanite, 'P', lo_pressures)
+    sill_ky_temperatures = find_univariant(composition, [sillimanite], kyanite, 'P', hi_pressures)
 
-    # To solve for the positions of the reaction lines, we need to change the assemblages
-    # and constraints, as only two phases are stable.
-    for i, P in enumerate(lo_pressures):
-        constraints= [['P', P], ['X', [1., 0.], [1., 1.], 0.]]
-    
-        assemblage = burnman.Composite([andalusite, kyanite])
-        and_ky_temperatures[i] = gibbs_minimizer(composition, assemblage, constraints)['T']
-        
-        assemblage = burnman.Composite([andalusite, sillimanite])
-        and_sill_temperatures[i] = gibbs_minimizer(composition, assemblage, constraints)['T']
-    
-    for i, P in enumerate(hi_pressures):
-        constraints= [['P', P], ['X', [1., 0.], [1., 1.], 0.]]
-        
-        assemblage = burnman.Composite([sillimanite, kyanite])
-        sill_ky_temperatures[i] = gibbs_minimizer(composition, assemblage, constraints)['T']
-    
     plt.plot(and_ky_temperatures, lo_pressures/1.e9, label='and-ky')
     plt.plot(and_sill_temperatures, lo_pressures/1.e9, label='and-sill')
     plt.plot(sill_ky_temperatures, hi_pressures/1.e9, label='sill-ky')
     
-
     plt.title('Aluminosilicate phase diagram')
     plt.xlabel('Temperature (K)')
     plt.ylabel('Pressure (GPa)')
@@ -117,7 +83,9 @@ if __name__ == "__main__":
     # Example 2: The olivine phase diagram at 1400 K
     # Our aim here is to plot the X-P phase diagram
     # for Mg2SiO4-Fe2SiO4 olivine at a fixed temperature
-
+    # For this example we illustrate the lower level functions
+    # behind find_invariant and find_univariant
+    
     # Here we define the temperature and assemblage
     T = 1400.
     
@@ -137,12 +105,29 @@ if __name__ == "__main__":
     # and the proportions of wadsleyite and ringwoodite to zero, and
     # find the bulk composition (which much therefore correspond to
     # the composition of olivine) at which all three phases coexist.
+
+    # The gibbs minimizer takes compositional
+    # constraints as a list with the first element being the string 'X'.
+    # The second two elements are the multipliers for the compositional vector,
+    # which has the form amount of phase 0, fraction of endmember 1 in phase 0,
+    # fraction of endmember 2 in phase 0 ... amount of phase i ...
+    # In this case, we are dealing with pure phases, so the compositional
+    # vector is the amount of andalusite, sillimanite and kyanite.
+    # These do not have to sum to one. The compositional contraints are
+    # expressed in the form:
+    # dot(constraint[1],cvector)/dot(constraint[2],cvector) = constraint[3]
+    # For the invariant point, the two constraints are that the proportion
+    # of two of the phases goes to zero.
+    
     constraints = [['T', T],
                    ['X', [0., 0., 1., 0., 0., 0.], [1., 0., 1., 0., 1., 0.], 0.],
                    ['X', [0., 0., 0., 0., 1., 0.], [1., 0., 1., 0., 1., 0.], 0.]]
+
+
+    # Here we call the gibbs bulk minimizer.
     sol = gibbs_bulk_minimizer(composition1, composition2, 0.25, assemblage, constraints)
 
-    # The output of the bulk minimizer is a list containing P, T, X
+    # The output of the bulk minimizer is a dictionary containing P, T, X
     # (where X is the distance along the compositional binary),
     # followed by the compositional vector
     P_inv = sol['P']
@@ -175,6 +160,9 @@ if __name__ == "__main__":
     # For a given composition, find the pressure at which the
     # second phase of each binary loop becomes stable,
     # and the composition of the phase at that point.
+
+    # Because each calculation is at a constant composition, we use the
+    # function gibbs_minimizer.
     for i, assemblage in enumerate(assemblages):
         x1 = np.linspace(*c_bounds[i])
         x2 = np.empty_like(x1)
@@ -234,6 +222,20 @@ if __name__ == "__main__":
     plt.plot(pressures, x_fper, label='x(Fe) fper')
     plt.legend(loc='upper right')
     plt.show()
+    
+    '''
+    # Example 4: Solvus in pyrope-grossular
 
+    
+    composition = { 'Na': 0., 'Ca': 1.5, 'Fe': 0., 'Mg': 1.5, 'Al': 2., 'Si': 3., 'O': 12.}
+    garnet = SLB_2011.garnet()
+    assemblage = burnman.Composite([garnet, garnet])
 
+    P = 1.e5
+    temperatures = np.linspace(300., 800., 21)
 
+    for i, T in enumerate(temperatures):
+        constraints=[['P', P], ['T', T]]
+        sol = gibbs_minimizer(composition, assemblage, constraints, guesses=[0.5, 0., 0., 0., 0., 0.5, 0., 1., 0., 0.])
+        
+    '''
