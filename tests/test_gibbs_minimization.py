afd0c1dbe1edddfcb12bb39bbd54d313232ec096
Bob Myhill <myhill.bob@gmail.com>
2016-04-28 18:37:29 +0100

    added unit tests

---
+++ tests/test_gibbs_minimization.py
@@ -0,0 +1,52 @@
+from __future__ import absolute_import
+
+import os
+import sys
+sys.path.insert(1, os.path.abspath('..'))
+import warnings
+
+import unittest
+from util import BurnManTest
+
+from burnman import Composite
+from burnman import tools
+from burnman import minerals
+from burnman.equilibriumassemblage import *
+
+
+fo = minerals.HP_2011_ds62.fo()
+fa = minerals.HP_2011_ds62.fa()
+sillimanite = minerals.HP_2011_ds62.sill()
+andalusite = minerals.HP_2011_ds62.andalusite()
+kyanite = minerals.HP_2011_ds62.ky()
+
+assemblage = Composite([sillimanite, andalusite])
+
+assemblage2 = Composite([minerals.SLB_2011.mg_fe_olivine(), minerals.SLB_2011.mg_fe_wadsleyite()])
+
+class Minimization(BurnManTest):
+    
+    def test_minimizer(self):
+        sol = gibbs_minimizer(sillimanite.params['formula'], assemblage, [['T', 800.], ['X', [1., 0.], [1., 1.], 0.]])
+        self.assertArraysAlmostEqual((sol['c'] - np.array([0., 1.]))/1.e6, [0., 0.])
+ 
+    def test_bulk_minimizer(self):
+        constraints = [['T', 1600.], ['P', 8.e9], ['X', [0., 0., 1., 0.], [1., 0., 1., 0.], 0.]]
+        sol = gibbs_bulk_minimizer(fo.params['formula'], fa.params['formula'], 0.71, assemblage2, constraints)
+        self.assertFloatEqual(sol['c'][1], sol['X'])
+    
+    def test_invariant(self):
+        PT0 = tools.invariant_point([sillimanite, andalusite], [1., -1.],
+                                    [sillimanite, kyanite], [1., -1.],
+                                    [0.4e9, 800.])
+
+        PT1 = find_invariant(sillimanite.params['formula'], [andalusite], [sillimanite, kyanite])
+        self.assertArraysAlmostEqual(PT0, PT1)
+    
+    def test_univariant(self):
+        P0 = tools.equilibrium_pressure([sillimanite, andalusite], [1., -1.], 800., 1.e9)
+        P1 = find_univariant(sillimanite.params['formula'], [sillimanite], andalusite, 'T', [800.])[0]
+        self.assertFloatEqual(P0, P1)
+
+if __name__ == '__main__':
+    unittest.main()
