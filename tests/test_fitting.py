b1538268423c39c294b80ac11732055cb53fc18b
Bob Myhill <myhill.bob@gmail.com>
2018-09-10 13:45:46 +0100

    added standard nonlinear test for fitting

--- tests/test_fitting.py
+++ tests/test_fitting.py
@@ -11,7 +11,7 @@ from burnman.nonlinear_fitting import *
 class test_fitting(BurnManTest):
 
     def test_linear_fit(self):
-        
+        # Test from Neri et al. (Meas. Sci. Technol. 1 (1990) 1007-1010.)
         i, x, Wx, y, Wy = np.loadtxt('../burnman/data/input_fitting/Pearson_York.dat', unpack=True)
 
         data = np.array([x, y]).T
@@ -45,6 +45,49 @@ class test_fitting(BurnManTest):
                                     param_tolerance = 1.e-5)
         
         self.assertArraysAlmostEqual([fitted_curve.WSS], [11.8663531941])
+
+
+    def test_polynomial_fit(self):
+        # Test from Neri et al. (Meas. Sci. Technol. 1 (1990) 1007-1010.)
+        i, x, Wx, y, Wy = np.loadtxt('../burnman/data/input_fitting/Pearson_York.dat', unpack=True)
+
+        data = np.array([x, y]).T
+        cov = np.array([[1./Wx, 0.*Wx], [0.*Wy, 1./Wy]]).T
+
+        class m():
+            def __init__(self, data, cov, guessed_params, delta_params):
+                self.data = data
+                self.data_covariances = cov
+                self.set_params(guessed_params)
+                self.delta_params = delta_params
+                self.mle_tolerances = np.array([1.e-8] * len(data[:,0]))
+
+            def set_params(self, param_values):
+                self.params = param_values
+        
+            def get_params(self):
+                return self.params
+    
+            def function(self, x, flag):
+                return np.array([x[0],
+                                 self.params[0]*x[0]*x[0]*x[0] +
+                                 self.params[1]*x[0]*x[0] +
+                                 self.params[2]*x[0] +
+                                 self.params[3]])
+
+            def normal(self, x, flag):
+                n = np.array([3.*self.params[0]*x[0]*x[0] +
+                              2.*self.params[1]*x[0] + 
+                              1.*self.params[2],
+                              -1.])
+                return n/np.linalg.norm(n)
+
+        guessed_params = np.array([-1.2e-2, 0.161, -1.15, 6.142])
+        delta_params = np.array([1.e-5, 1.e-5, 1.e-5, 1.e-5]) 
+        fitted_curve = m(data, cov, guessed_params, delta_params)
+        nonlinear_least_squares_fit(model=fitted_curve,
+                                    param_tolerance = 1.e-5)
+        self.assertArraysAlmostEqual([fitted_curve.WSS], [10.486904577])
 
     def test_fit_PVT_data(self):
         fo = burnman.minerals.HP_2011_ds62.fo()
