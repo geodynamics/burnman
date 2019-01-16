fixed formula, hessian calculation

burnman/solutionmodel.py
@@ -408,10 +408,11 @@ class IdealSolution (SolutionModel):
 
     def _log_ideal_activity_derivatives(self, molar_fractions):
         site_occupancies = np.dot(molar_fractions, self.endmember_occupancies)
+        n_site_atoms = self.endmember_occupancies[0].dot(self.site_multiplicities)
         dlnadp = np.einsum('ij, j, mj',
                            self.endmember_occupancies,
                            self.site_multiplicities*inverseish(site_occupancies),
-                           self.endmember_occupancies)
+                           self.endmember_occupancies) - n_site_atoms
         return dlnadp
 
 