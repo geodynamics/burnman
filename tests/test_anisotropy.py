170efbb57adf62b6bb202a44c5c730e9ee3b795e
Bob Myhill <myhill.bob@gmail.com>
2021-09-10 14:06:40 +0100

    add isentropic qualifier to AnisotropicMaterial

--- tests/test_anisotropy.py
+++ tests/test_anisotropy.py
@@ -17,28 +17,35 @@ class test_anisotropy(BurnManTest):
         m = anisotropy.IsotropicMaterial(rho, elastic_constants)
 
         E = G*(3.*l + 2.*G)/(l+G)
-        Vp = np.sqrt((m.bulk_modulus_reuss + 4./3.*m.shear_modulus_reuss)/rho)
+        Vp = np.sqrt((m.isentropic_bulk_modulus_reuss
+                      + 4./3.*m.shear_modulus_reuss)/rho)
         Vs = np.sqrt(m.shear_modulus_reuss/rho)
 
-        array_1 = [m.bulk_modulus_reuss, m.bulk_modulus_vrh,
-                   m.bulk_modulus_voigt, m.shear_modulus_reuss,
-                   m.shear_modulus_vrh, m.shear_modulus_voigt,
-                   m.universal_elastic_anisotropy + 6., m.isotropic_poisson_ratio,
+        array_1 = [m.isentropic_bulk_modulus_reuss,
+                   m.isentropic_bulk_modulus_vrh,
+                   m.isentropic_bulk_modulus_voigt,
+                   m.shear_modulus_reuss,
+                   m.shear_modulus_vrh,
+                   m.shear_modulus_voigt,
+                   m.isentropic_universal_elastic_anisotropy + 6.,
+                   m.isentropic_isotropic_poisson_ratio,
                    E, Vp, Vs, Vs]
 
         d1 = [1., 0., 0.]
         d2 = [0., 1., 0.]
 
-        beta_100 = m.linear_compressibility(direction=d1)
-        E_100 = m.youngs_modulus(direction=d1)
+        beta_100 = m.isentropic_linear_compressibility(direction=d1)
+        E_100 = m.isentropic_youngs_modulus(direction=d1)
         G_100_010 = m.shear_modulus(plane_normal=d1, shear_direction=d2)
-        nu_100_010 = m.poissons_ratio(axial_direction=d1, lateral_direction=d2)
-        wave_speeds, wave_directions = m.wave_velocities(
-            propagation_direction=d1)
+        nu_100_010 = m.isentropic_poissons_ratio(axial_direction=d1,
+                                                 lateral_direction=d2)
+        wave_speeds, wave_directions = m.wave_velocities(propagation_direction=d1)
         Vp, Vs1, Vs2 = wave_speeds
 
-        array_2 = [1./3./beta_100, 1./3./beta_100,
-                   1./3./beta_100, G_100_010,
+        array_2 = [1./3./beta_100,
+                   1./3./beta_100,
+                   1./3./beta_100,
+                   G_100_010,
                    G_100_010, G,
                    6., nu_100_010,
                    E_100, Vp, Vs1, Vs2]
@@ -65,20 +72,22 @@ class test_anisotropy(BurnManTest):
 
     def test_tetragonal_ii(self):
         m = anisotropy.TetragonalMaterial(3000., [1., 2., 3., 4., 5., 6., 7.])
-        self.assertFloatEqual(
-            m.stiffness_tensor[0][5], -m.stiffness_tensor[1][5])
+        self.assertFloatEqual(m.isentropic_stiffness_tensor[0][5],
+                              -m.isentropic_stiffness_tensor[1][5])
 
     def test_rhombohedral_i(self):
-        m = anisotropy.RhombohedralMaterial(
-            3000., [1., 2., 3., 4., 5., 6., 7.])
-        self.assertFloatEqual(
-            m.stiffness_tensor[0][3], -m.stiffness_tensor[1][3])
+        m = anisotropy.RhombohedralMaterial(3000.,
+                                            [1., 2., 3., 4., 5., 6., 7.])
+        self.assertFloatEqual(m.isentropic_stiffness_tensor[0][3],
+                              -m.isentropic_stiffness_tensor[1][3])
 
     def test_rhombohedral_ii(self):
-        m = anisotropy.RhombohedralMaterial(
-            3000., [1., 2., 3., 4., 5., 6., 7., 8.])
-        self.assertArraysAlmostEqual([m.stiffness_tensor[0][3], m.stiffness_tensor[0][4]],
-                                     [-m.stiffness_tensor[1][3], -m.stiffness_tensor[1][4]])
+        m = anisotropy.RhombohedralMaterial(3000.,
+                                            [1., 2., 3., 4., 5., 6., 7., 8.])
+        self.assertArraysAlmostEqual([m.isentropic_stiffness_tensor[0][3],
+                                      m.isentropic_stiffness_tensor[0][4]],
+                                     [-m.isentropic_stiffness_tensor[1][3],
+                                      -m.isentropic_stiffness_tensor[1][4]])
 
 
 if __name__ == '__main__':
