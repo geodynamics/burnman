ef94fd393f0ffde0603bb39bb1f228a4071dbcd9
Bob Myhill <myhill.bob@gmail.com>
2021-10-11 22:50:21 +0100

    zog update

--- contrib/burnman_paper/build_zog.py
+++ contrib/burnman_paper/build_zog.py
@@ -4,20 +4,16 @@ from __future__ import print_function
 import matplotlib.pyplot as plt
 import numpy as np
 
-from scipy.integrate import quad
-from scipy.interpolate import UnivariateSpline
 
 import burnman_path  # adds the local burnman directory to the path
 import burnman
 from burnman import minerals
 from burnman import Layer, Planet, Mineral, PerplexMaterial
-
+from burnman import BoundaryLayerPerturbation
 import warnings
-
-assert burnman_path  # silence pyflakes warning
-
 import urllib.request
 
+assert burnman_path  # silence pyflakes warning
 
 icb_radius = 1220.e3
 inner_core = Layer('inner core', radii=np.linspace(0., icb_radius, 21))
@@ -55,9 +51,9 @@ urllib.request.urlretrieve("https://raw.githubusercontent.com/"
                            "pyrolite_perplex_table.dat")
 
 
-lab_radius = 6041.e3
+lab_radius = 6001.e3
 moho_radius = 6341.e3
-lab_temperature = 1600. - 90.
+lab_temperature = 1645. - 119.5  # to be consistent with TBL, defined below
 moho_temperature = 620.
 surface_temperature = 300.
 
@@ -71,20 +67,22 @@ convecting_mantle.set_material(pyrolite)
 # lower mantle has a Rayleigh number of 1.e7, and that there
 # is an 840 K jump across the basal thermal boundary layer and an
 # 84 K jump at the top of the lower mantle.
-tbl_perturbation = burnman.BoundaryLayerPerturbation(radius_bottom=cmb_radius,
-                                                     radius_top=lab_radius,
-                                                     rayleigh_number=1.e7,
-                                                     temperature_change=840.+90.,
-                                                     boundary_layer_ratio=90./840.)
+tbl_perturbation = BoundaryLayerPerturbation(radius_bottom=cmb_radius,
+                                             radius_top=lab_radius,
+                                             rayleigh_number=1.e7,
+                                             temperature_change=0.,  # tweaked
+                                             boundary_layer_ratio=0.)
 dTdr_top = ((moho_temperature - lab_temperature)
             / (moho_radius - lab_radius))
-tbl_perturbation.set_model_thermal_gradients(-18.e-3, dTdr_top)
+tbl_perturbation.set_model_thermal_gradients(-19.e-3, dTdr_top)
 
 lower_mantle_tbl = tbl_perturbation.temperature(convecting_mantle_radii)
+
+print(lower_mantle_tbl[0], lower_mantle_tbl[-1])
 convecting_mantle.set_temperature_mode('perturbed-adiabatic',
                                        temperatures=lower_mantle_tbl)
 
-dunite = minerals.SLB_2011.mg_fe_olivine(molar_fractions=[0.915, 0.085])
+dunite = minerals.SLB_2011.mg_fe_olivine(molar_fractions=[0.92, 0.08])
 lithospheric_mantle = Layer('lithospheric mantle',
                             radii=np.linspace(lab_radius, moho_radius, 31))
 lithospheric_mantle.set_material(dunite)
@@ -130,10 +128,18 @@ with warnings.catch_warnings(record=True) as w:
     premdensity, prempressure, premgravity, premvs, premvp = eval
     print(w[-1].message)
 
-
-
 fig = plt.figure(figsize=(8, 5))
 ax = [fig.add_subplot(2, 2, i) for i in range(1, 5)]
+
+
+bounds = np.array([[layer.radii[0]/1.e3, layer.radii[-1]/1.e3]
+                   for layer in planet_zog.layers])
+maxy = [15, 400, 12, 4000]
+for bound in bounds:
+    for i in range(4):
+        ax[i].fill_betweenx([0., maxy[i]],
+                            [bound[0], bound[0]],
+                            [bound[1], bound[1]], alpha=0.2)
 
 ax[0].plot(planet_zog.radii / 1.e3, planet_zog.density / 1.e3,
            label=planet_zog.name)
@@ -154,7 +160,7 @@ ax[2].set_xlabel('Radius (km)')
 
 # Make a subplot showing the calculated temperature profile
 ax[3].plot(planet_zog.radii / 1.e3, planet_zog.temperature)
-ax[3].set_ylabel('Temperature ($K$)')
+ax[3].set_ylabel('Temperature (K)')
 ax[3].set_xlabel('Radius (km)')
 ax[3].set_ylim(0.,)
 
@@ -162,6 +168,8 @@ for i in range(2):
     ax[i].set_xticklabels([])
 for i in range(4):
     ax[i].set_xlim(0., max(planet_zog.radii) / 1.e3)
+    ax[i].set_ylim(0., maxy[i])
 
 fig.tight_layout()
+fig.savefig('figures/zog.pdf')
 plt.show()
