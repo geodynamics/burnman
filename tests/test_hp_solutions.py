06c29fc139207091ea6fb44098506408714806c5
Bob Myhill <myhill.bob@gmail.com>
2024-05-29 10:42:11 -0600

    added HP basic and pelitic solutions

--- tests/test_hp_solutions.py
+++ tests/test_hp_solutions.py
@@ -3,7 +3,15 @@ from __future__ import print_function
 import unittest
 from util import BurnManTest
 from burnman import Mineral, CombinedMineral, Solution
-from burnman.minerals import ig50NCKFMASHTOCr, ig50NCKFMASTOCr
+from burnman.minerals import (
+    ig50NCKFMASHTOCr,
+    ig50NCKFMASTOCr,
+    mb50NCKFMASHTO,
+    mp50KFMASH,
+    mp50MnNCKFMASHTO,
+    mp50NCKFMASHTO,
+)
+
 import inspect
 import numpy as np
 from collections import Counter
@@ -30,6 +38,74 @@ class hp_solutions(BurnManTest):
     def test_ig50NCKFMASTOCr(self):
         for m in dir(ig50NCKFMASTOCr):
             mineral = getattr(ig50NCKFMASTOCr, m)
+            if (
+                inspect.isclass(mineral)
+                and mineral != Mineral
+                and mineral != CombinedMineral
+                and issubclass(mineral, Mineral)
+            ):
+                if issubclass(mineral, Solution) and mineral is not Solution:
+                    mbr_names = mineral().endmember_names
+                    n_mbrs = len(mbr_names)
+                    molar_fractions = np.ones(n_mbrs) / n_mbrs
+                    m = mineral()
+                    m.set_composition(molar_fractions)
+                    self.assertTrue(type(m.formula) is Counter)
+
+    def test_mb50NCKFMASHTO(self):
+        for m in dir(mb50NCKFMASHTO):
+            mineral = getattr(mb50NCKFMASHTO, m)
+            if (
+                inspect.isclass(mineral)
+                and mineral != Mineral
+                and mineral != CombinedMineral
+                and issubclass(mineral, Mineral)
+            ):
+                if issubclass(mineral, Solution) and mineral is not Solution:
+                    mbr_names = mineral().endmember_names
+                    n_mbrs = len(mbr_names)
+                    molar_fractions = np.ones(n_mbrs) / n_mbrs
+                    m = mineral()
+                    m.set_composition(molar_fractions)
+                    self.assertTrue(type(m.formula) is Counter)
+
+    def test_mp50KFMASH(self):
+        for m in dir(mp50KFMASH):
+            mineral = getattr(mp50KFMASH, m)
+            if (
+                inspect.isclass(mineral)
+                and mineral != Mineral
+                and mineral != CombinedMineral
+                and issubclass(mineral, Mineral)
+            ):
+                if issubclass(mineral, Solution) and mineral is not Solution:
+                    mbr_names = mineral().endmember_names
+                    n_mbrs = len(mbr_names)
+                    molar_fractions = np.ones(n_mbrs) / n_mbrs
+                    m = mineral()
+                    m.set_composition(molar_fractions)
+                    self.assertTrue(type(m.formula) is Counter)
+
+    def test_mp50MnNCKFMASHTO(self):
+        for m in dir(mp50MnNCKFMASHTO):
+            mineral = getattr(mp50MnNCKFMASHTO, m)
+            if (
+                inspect.isclass(mineral)
+                and mineral != Mineral
+                and mineral != CombinedMineral
+                and issubclass(mineral, Mineral)
+            ):
+                if issubclass(mineral, Solution) and mineral is not Solution:
+                    mbr_names = mineral().endmember_names
+                    n_mbrs = len(mbr_names)
+                    molar_fractions = np.ones(n_mbrs) / n_mbrs
+                    m = mineral()
+                    m.set_composition(molar_fractions)
+                    self.assertTrue(type(m.formula) is Counter)
+
+    def test_mp50NCKFMASHTO(self):
+        for m in dir(mp50NCKFMASHTO):
+            mineral = getattr(mp50NCKFMASHTO, m)
             if (
                 inspect.isclass(mineral)
                 and mineral != Mineral
